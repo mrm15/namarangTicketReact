fe7afb821e206824c4787eb00216688d63f34818
mrm15 <mrm.learn@gmail.com>
2024-02-01 11:14:20 +0330

    Update DatePicker.tsx

--- src/Components/MyFormik/DatePicker.tsx
+++ src/Components/MyFormik/DatePicker.tsx
@@ -1,32 +1,46 @@
 import React from 'react'
-import DateView from 'react-datepicker'
 import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
-import 'react-datepicker/dist/react-datepicker.css'
+
+import FormDatePicker from "../MyDatePicker";
 
 function DatePicker (props) {
   const { label, name, ...rest } = props
-  return (
-    <div className='form-control'>
-      <label htmlFor={name}>{label}</label>
-      <Field name={name}>
-        {({ form, field }) => {
-          const { setFieldValue } = form
-          const { value } = field
-          return (
-            <DateView
-              id={name}
-              {...field}
-              {...rest}
-              selected={value}
-              onChange={val => setFieldValue(name, val)}
-            />
-          )
-        }}
-      </Field>
-      <ErrorMessage component={TextError} name={name} />
-    </div>
-  )
+
+    try {
+        return (
+            <div className='form-control'>
+                <label htmlFor={name}>{label}</label>
+                <Field name={name}>
+                    {({ form, field }) => {
+                        const { setFieldValue } = form
+                        const { value } = field
+                        return (
+                            <>
+                                <FormDatePicker
+                                    id={name}
+                                    {...field}
+                                    {...rest}
+                                    value={value}
+                                    onChange={val => setFieldValue(name, val)}
+                                />
+                                {/*<DateView*/}
+                                {/*    id={name}*/}
+                                {/*    {...field}*/}
+                                {/*    {...rest}*/}
+                                {/*    selected={value}*/}
+                                {/*    onChange={val => setFieldValue(name, val)}*/}
+                                {/*/>*/}
+                            </>
+                        )
+                    }}
+                </Field>
+                <ErrorMessage component={TextError} name={name} />
+            </div>
+        )
+    }catch (error){
+      return <>{error?.toString()}</>
+    }
 }
 
 export default DatePicker
