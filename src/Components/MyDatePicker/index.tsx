e8e22e62303af94fb085131d2a220a237e5a123c
mrm15 <mrm.learn@gmail.com>
2024-02-01 11:14:51 +0330

    Create index.tsx

---
+++ src/Components/MyDatePicker/index.tsx
@@ -0,0 +1,67 @@
+import  {useEffect, useRef} from 'react';
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import DatePicker from "react-multi-date-picker";
+import numeric from "../../utils/NumericFunction";
+
+const FormDatePicker = (props) => {
+
+    const {value, onChange} = props
+
+    const dateCal = useRef()
+    useEffect(() => {
+
+
+
+        const handleKeyDown = (event) => {
+            if (event?.keyCode === 9) {
+                dateCal?.current?.closeCalendar();
+            }
+        };
+
+        dateCal?.current?.addEventListener("keydown", handleKeyDown);
+        // dateCal?.current?.addEventListener("focusout", handleBlur);
+
+        // Cleanup function
+        return () => {
+            dateCal?.current?.removeEventListener("keydown", handleKeyDown);
+            // dateCal?.current?.removeEventListener("focusout", handleBlur);
+
+        };
+    }, [dateCal]);
+
+
+    return (<div>
+        <DatePicker
+            ref={dateCal}
+
+            onChange={(date) => {
+
+
+                if (date) {
+                    const newValue = numeric.p2e(date.toLocaleString())
+                    onChange(newValue)
+
+                    // تاریخ رو همینجا ولیدیت میکنم چون آن بلر اینجا فایر نمیشه
+                    // changeValueHandler(id, 'errorMessage', '')
+
+                } else {
+                    onChange('')
+
+
+
+                }
+            }}
+            calendar={persian}
+            placeholder="تاریخ"
+            locale={persian_fa}
+            value={value}
+            calendarPosition="bottom-right"
+            hideOnScroll
+
+
+        />
+    </div>);
+};
+
+export default FormDatePicker;
