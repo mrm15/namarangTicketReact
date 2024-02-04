2716bb559973be479d107e3dcbfc6e0a1744cd56
mrm15 <mrm.learn@gmail.com>
2024-02-04 10:30:02 +0330

    upload single File

--- src/Components/MyFormik/FileInput.tsx
+++ src/Components/MyFormik/FileInput.tsx
@@ -1,10 +1,14 @@
 
 export const FileInput = ({ field, form, ...rest }) => {
     const handleChange = (e) => {
+
+
         const files = e.target.files;
         let myFiles = Array.from(files)
+        myFiles = myFiles[0]
         // Use the name attribute of the field to update its value
-        form.setFieldValue(field.name, myFiles.length > 0 ? myFiles : '')
+        // form.setFieldValue(field.name, myFiles.length > 0 ? myFiles : '')
+        form.setFieldValue(field.name, myFiles)
     }
 
     return <input type="file" {...rest} onChange={handleChange} />;
