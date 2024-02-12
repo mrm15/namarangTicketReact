e68fd44311d83ffb4e006708ffb76de2d508b712
mrm15 <mrm.learn@gmail.com>
2024-02-12 10:08:31 +0330

    control if input is disabled ( on edit  mode   disable unique sections)

--- src/Components/MyFormik/Input.tsx
+++ src/Components/MyFormik/Input.tsx
@@ -3,11 +3,12 @@ import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
 
 function Input (props) {
-  const { label, name, ...rest } = props
+
+    const { label, name,disabled, ...rest } = props
   return (
     <div className='div__group__input_select'>
       <label htmlFor={name}>{label}</label>
-      <Field id={name} name={name} {...rest} />
+      <Field  id={name} name={name} {...rest} disabled={disabled} />
       <ErrorMessage component={TextError} name={name} />
     </div>
   )
