80ae208406664a0ba3228c79ddb0a3f757694343
mrm15 <mrm.learn@gmail.com>
2024-02-12 10:09:10 +0330

    add  (select one option) to select

--- src/Components/MyFormik/Select.tsx
+++ src/Components/MyFormik/Select.tsx
@@ -8,13 +8,18 @@ function Select(props) {
         return (
             <div className='div__group__input_select'>
                 <label htmlFor={name}>{label}</label>
-                <Field as='select' id={name} name={name} {...rest}>
+                <Field as={'select'} id={name} name={name} {...rest}>
+                    <option value={''} >انتخاب کنید</option>
                     {options?.map(option => {
-                        return (
-                            <option key={option.value} value={option.value}>
-                                {option.key}
-                            </option>
-                        )
+                        try {
+                            return (
+                                <option key={option.value} value={option.value}>
+                                    {option.key}
+                                </option>
+                            )
+                        } catch (error) {
+                            return <>{error?.toString()}</>
+                        }
                     })}
                 </Field>
                 <ErrorMessage component={TextError} name={name}/>
