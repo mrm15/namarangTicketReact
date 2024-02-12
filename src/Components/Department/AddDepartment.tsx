6426486c108d58715e575ecf7a2f7ac2d2de3f36
mrm15 <mrm.learn@gmail.com>
2024-02-12 18:48:19 +0330

    add department

---
+++ src/Components/Department/AddDepartment.tsx
@@ -0,0 +1,136 @@
+import {useEffect, useState} from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import {formikFormAddDepartment, validationSchemaAddUser} from "./addDepartmentFormikForm.ts";
+import Loader from "../Loader";
+import MyFormik from "../MyFormik";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate.tsx";
+
+const MyComponent = props => {
+    const [isLoading, setIsLoading] = useState(true)
+
+    const myLocation = useLocation()
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const editMode = !!myLocation?.state?.data;
+    const formUniqId = myLocation?.state?.data?.id
+
+    const text = {
+        title: editMode ? 'ویرایش دپارتمان  ' : 'افزودن دپارتمان جدید',
+        subTitle: editMode ? `ویرایش دپارتمان با شناسه ${formUniqId}_ ${myLocation.state.data?.name}` : `افزودن دپارتمان جدید به سایت`
+    }
+
+    const requestUrl = editMode ? `department/update` : 'department/create'
+
+    const [myFormikFormAddUser, setMyFormikFormAddUser] = useState([])
+    const [myInitialValuesAddUser, setMyInitialValuesAddUser] = useState({})
+    // ///////////////////////////////////////////
+
+    //////////////////////////////
+
+
+    // @ts-ignore
+    const {auth} = useAuth();
+
+    const myPrivateAxios = useAxiosPrivate()
+    const getArrayList = async (url) => {
+        const res = await myPrivateAxios.get(url);
+        return res.data.list || []
+    }
+
+
+    useEffect(() => {
+        let updatedFormConfig = [...formikFormAddDepartment]
+
+
+        if (editMode) {
+            updatedFormConfig = updatedFormConfig.map(v => {
+                const row = {...v}
+                if (row.name === 'name') {
+                    row['disabled'] = true
+                }
+                return row
+            })
+        }
+
+
+        const temp:any = {};
+        if (editMode) {
+            temp.id = formUniqId
+        }
+
+        for (const row of updatedFormConfig) {
+
+
+            if (editMode) {
+                temp[row.name] = myLocation?.state?.data[row.name]
+            } else {
+                temp[row.name] = ''
+            }
+        }
+
+        void getArrayList('/user/userList').then(userList => {
+            void getArrayList('/department/departmentList').then(departmentList => {
+                updatedFormConfig = updatedFormConfig.map(r => {
+                    const row = {...r}
+                    if (row.name === 'managerUserId') {
+                        row.options = userList
+                    }
+
+                    if (row.name === 'parentDepartmentId') {
+                        row.options = departmentList
+                    }
+                    return row
+                })
+                setMyInitialValuesAddUser(temp)
+                setMyFormikFormAddUser(updatedFormConfig);
+                setIsLoading(false);
+            })
+        })
+
+        // }, 1000);
+    }, [auth?.userInfo?.roleAccessList]); // Depend on user's role list to re-evaluate form config on change
+
+
+    const navigateTo = useNavigate()
+
+    let afterSubmit = undefined
+
+    if (editMode) {
+        afterSubmit = () => {
+            navigateTo(-1)
+        }
+    }
+
+    try {
+
+        return (<div className={'flex flex-wrap'}>
+            <div className="w-full text-center bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+                 role="alert">
+                <p className="font-bold">{text.title}</p>
+                <p className="text-sm">{text.subTitle}</p>
+            </div>
+
+            {isLoading ? <Loader type={1}/> :
+                <>
+                    <MyFormik
+                        formikForm={myFormikFormAddUser}
+                        initialValues={myInitialValuesAddUser}
+                        validationSchema={validationSchemaAddUser}
+                        afterSubmit={afterSubmit}
+                        requestUrl={requestUrl}
+                    />
+
+
+                </>
+            }
+        </div>);
+    } catch (error) {
+        return <>{error?.toString()}</>
+    }
+
+
+};
+
+MyComponent.propTypes = {};
+
+export default MyComponent;
---
+++ src/Components/Department/addDepartmentFormikForm.ts
@@ -0,0 +1,56 @@
+import * as Yup from "yup";
+import {STRINGS} from "../../utils/STRINGS.ts";
+// eslint-disable-next-line react-hooks/rules-of-hooks
+
+const checkboxOptions = {key: 'تعیین وضعیت', value: "1"}
+
+
+const validationSchemaAddUser = Yup.object({
+    // email: Yup.string().email('Invalid email format').required('Required'),
+
+    name: Yup.string().required(STRINGS.REQUIRED),
+    // course: Yup.string().required('Required'),
+    // courseDate: Yup.date().required('Required').nullable()
+
+})
+
+const isActiveRadioOptions = [
+    {key: 'بله', value: 'true'},
+    {key: 'خیر', value: 'false'},
+]
+const formikFormAddDepartment = [
+
+
+
+
+
+
+    {control: 'input' , name:'name', label: 'نام دپارتمان' , options: null},
+    {control: 'input' , name:'description', label: 'توضیحات' , options: null},
+    {control: 'select' , name:'managerUserId', label: 'مدیر دپارتمان' , options: null},
+    {control: 'select' , name:'parentDepartmentId', label: 'دپارتمان والد' , options: null},
+    {control: 'input' , name:'location', label: 'لوکیشن' , options: null},
+    {control: 'textarea' , name:'address', label: 'آدرس' , options: null},
+    {control: 'input' , name:'phoneNumber', label: 'شماره تماس' , options: null},
+    {control: 'input' , name:'emailAddress', label: 'ایمیل' , options: null},
+    {control: 'input' , name:'contactInfo', label: 'اطلاعات تماس' , options: null},
+    {control: 'color' , name:'departmentTaskColor', label: 'رنگ تسک' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'departmentAccessToSendTicket', label: 'ارسال  تیکت توسط دپارتمان' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'departmentAccessToReplyTicket', label: 'ارسال پاسخ به تیکت های موجود' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'departmentAccessToArchiveTicket', label: 'دسترسی به تیکت آرشیو' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'departmentAccessToTaskSection', label: 'دسترسی به تسک' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'departmentAccessToArchiveTasks', label: 'آرشیو تسک ها' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'accessToSameDepartmentToAssignTask', label: 'ایجاد تسک برای همدپارتمانی ها' , options: isActiveRadioOptions},
+    {control: 'radio' , name:'accessToOtherUsersToAssignTask', label: 'ایجاد تسک برای همه' , options: isActiveRadioOptions},
+
+
+
+
+
+]
+
+export {
+    checkboxOptions,
+    validationSchemaAddUser,
+    formikFormAddDepartment
+}
