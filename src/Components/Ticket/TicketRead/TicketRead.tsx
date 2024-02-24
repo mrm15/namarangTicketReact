2712ba3b5b5711e03a493b63ae39d95a253fad64
mrm15 <mrm.learn@gmail.com>
2024-02-25 00:32:24 +0330

    Create TicketRead.tsx

---
+++ src/Components/Ticket/TicketRead/TicketRead.tsx
@@ -0,0 +1,240 @@
+import {PAGES} from "../../../Pages/Route-string";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
+import {toast} from "react-toastify";
+import EditButton from "../../../assets/icons/EditButton";
+import DeleteButton from "../../../assets/icons/DeleteButton";
+import Loader from "../../Loader";
+import AggridDataShow from "../../AgGridDataShow/AgGridDataShow";
+
+function TicketRead() {
+    const requestUrl = 'status/read'
+    const navigateEditPage = PAGES.STATUS_ADD_EDIT
+    const deleteRequest = 'status/delete/'
+
+    const CheckboxRenderer = (params) => {
+        const handleCheckboxClick = (e) => {
+            e.stopPropagation();
+            console.log("Checkbox clicked:", params.data);
+            // Additional logic
+        };
+
+        try {
+            return (
+                <input
+                    type="checkbox"
+                    checked={params.value}
+                    onClick={handleCheckboxClick}
+                />
+            );
+        } catch (error) {
+            return <>{error.toString()}</>
+        }
+    };
+
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [reload, setReload] = useState(1)
+
+
+    const navigateTo = useNavigate()
+    const editButtonHandler = (params) => {
+
+        const data = params.data
+        navigateTo(navigateEditPage, {state: {data}})
+    }
+
+    const myAxiosPrivate = useAxiosPrivate()
+
+    const handleDeleteUser = async (id: any) => {
+        const url = deleteRequest + id
+
+        try {
+            const response = await myAxiosPrivate.delete(url)
+            if (response?.data) {
+
+                toast.success(response?.data?.message)
+                setReload(ps => ps + 1)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const deleteButtonHandler = async (params) => {
+
+        const data = params.data;
+
+        const message = `آیا مطمئنی که میخوای وضعیت  با نام
+        ${data?.name}
+        به صورت کامل برای همیشه از لیست وضعیت  ها حذف کنی؟
+        `
+        const confirmResult1 = confirm(message)
+        if (confirmResult1) {
+            const message = ` برای بار دوم  عرض میکنم.  این فرآیند قابل برگشت نیست.
+            آیا مطمئنی که میخوای وضعیت  با نام
+        ${data?.name}
+        به صورت کامل برای همیشه از لیست وضعیت  ها حذف کنی؟
+        `
+            const confirmResult2 = confirm(message)
+            if (confirmResult2) {
+
+                await handleDeleteUser(data.id)
+            }
+        }
+    }
+
+    const myColumnDefs = [
+        // Add the new column with icon and click handler
+        {
+            headerName: "عملیات", cellRenderer: (params) => (
+                <div className={'flex flex-wrap gap-1 items-center justify-center'}>
+                    <button
+                        onClick={() => editButtonHandler(params)}
+                    >
+                        <EditButton/>
+                    </button>
+                    <button
+                        onClick={() => deleteButtonHandler(params)}
+
+                        className={'text-red-600'}>
+
+                        <DeleteButton/>
+                    </button>
+                </div>
+            ),
+            cellStyle: () => ({
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }),
+        },
+        /////////////////////
+        {headerName: "شماره تماس", field: "phoneNumber", minWidth: 150, hide: false},
+        {headerName: "نام", field: "name", minWidth: 150, hide: false},
+        {headerName: "دسترسی به مخاطبین", field: "addContactAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {
+            headerName: "ویرایش مخاطبین",
+            field: "editContactAccess",
+            minWidth: 150,
+            hide: false,
+            cellRenderer: CheckboxRenderer
+        },
+        {headerName: "حذف مخاطبین", field: "deleteContactAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {headerName: "مشاهده مخاطبین", field: "listAllContactAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {headerName: "مشاهده مخاطبین خودش", field: "listOwnContactAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {
+            headerName: "خروجی گرفتن از مخاطبین",
+            field: "exportContactAccess",
+            hide: false,
+            cellRenderer: CheckboxRenderer
+        },
+        {headerName: "افزودن وضعیت ", field: "addUserAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {headerName: "حذف وضعیت ", field: "deleteUserAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {headerName: "ویرایش وضعیت ", field: "editUserAccess", hide: false, cellRenderer: CheckboxRenderer},
+        {headerName: "مشاهده لیست وضعیت ", field: "listUserAccess", hide: false, cellRenderer: CheckboxRenderer},
+    ]
+
+    const [myTableData, setMyTableData] = useState({
+
+        columnDefs: [],
+        rowData: []
+    });
+
+    const addCustomColumn = (myHeaderArray: []) => {
+
+        const headerArray = [...myHeaderArray]
+
+        // @ts-ignore
+        headerArray.unshift({
+            headerName: "عملیات", cellRenderer: (params) => (
+                <div className={'flex flex-wrap gap-1 items-center justify-center'}>
+                    <button
+                        onClick={() => editButtonHandler(params)}
+                    >
+                        <EditButton/>
+                    </button>
+                    <button
+                        onClick={() => deleteButtonHandler(params)}
+
+                        className={'text-red-600'}>
+
+                        <DeleteButton/>
+                    </button>
+                </div>
+            ),
+            cellStyle: () => ({
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }),
+        });
+
+
+        return headerArray
+
+
+    }
+
+
+    useEffect(() => {
+
+
+        const getList = async () => {
+            const res = await myAxiosPrivate.get(requestUrl)
+            if (res.data) {
+
+
+                const tableData = res.data.list;
+
+                tableData.columnDefs = addCustomColumn(tableData.columnDefs)
+
+                // mohammad mrm
+
+
+                console.log(res.data)
+                setMyTableData(tableData)
+                setIsLoading(false)
+            }
+        }
+
+        void getList()
+
+
+    }, [reload]);
+
+    const onCellClicked = (params) => {
+        console.log(params.data)
+    }
+    try {
+        return (
+            <div>
+                <div className={'font-bold my-3 '}>
+                    <div
+                        className={'bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3'}
+                    >
+                        <div> لیست وضعیت </div>
+                        <div
+                            className={'flex flex-wrap justify-center items-center mx-2'}
+                        >
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+                {isLoading ? <Loader/> :
+                    <div>
+                        <AggridDataShow
+                            columnDefs={myTableData.columnDefs}
+                            rowData={myTableData.rowData}
+                            // onCellClicked={onCellClicked}
+                        />
+                    </div>}
+            </div>
+        );
+    } catch (error) {
+        return <>{error.toString()}</>
+    }
+}
