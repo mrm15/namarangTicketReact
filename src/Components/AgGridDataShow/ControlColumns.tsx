fae64e6286eaf78e2ad06f0bacd2320983f5cda5
mrm15 <mrm.learn@gmail.com>
2024-03-02 09:55:03 +0330

    add try chach to column ControlColumns.tsx

--- src/Components/AgGridDataShow/ControlColumns.tsx
+++ src/Components/AgGridDataShow/ControlColumns.tsx
@@ -5,22 +5,27 @@ import {v4 as uuidV4} from "uuid";
 const ControlColumns = ({columnDefs, onChange}) => {
 
 
-    return (
-        <div className={'w-full flex flex-wrap align-middle select-none'}>
-            {columnDefs?.map((row, index: Key) => {
-                const randomId = uuidV4()
-                const checked = (row.hide)
+    try {
+        return (
+            <div className={'w-full flex flex-wrap align-middle select-none'}>
+                {columnDefs?.map((row, index: Key) => {
+                    const randomId = uuidV4()
+                    const checked = (row.hide)
 
-                return <div key={index} className={'flex justify-center items-center mx-2'}>
-                    <input className={'mx-1'} id={randomId} type="checkbox"
-                           checked={!checked}
-                           onChange={e => onChange(e, row)}
-                    />
-                    <label htmlFor={randomId}>{row.headerName}</label>
-                </div>
-            })}
-        </div>
-    );
+                    return <div key={index} className={'flex justify-center items-center mx-2'}>
+                        <input className={'mx-1'} id={randomId} type="checkbox"
+                               checked={!checked}
+                               onChange={e => onChange(e, row)}
+                        />
+                        <label htmlFor={randomId}>{row.headerName}</label>
+                    </div>
+                })}
+            </div>
+        );
+    } catch (error: any) {
+        return <>{error.toString()}</>
+
+    }
 };
 
 export default ControlColumns;
