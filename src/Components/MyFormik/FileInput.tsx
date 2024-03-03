a7efd7931b5333d063b4d18a1b2db1d414aaabff
mrm15 <mrm.learn@gmail.com>
2024-03-03 12:14:45 +0330

    remove errors

--- src/Components/MyFormik/FileInput.tsx
+++ src/Components/MyFormik/FileInput.tsx
@@ -7,21 +7,21 @@ export const FileInput = (props) => {
     const [realod, setRealod] = useState(1)
     useEffect(() => {
         if (!(form?.values[field?.name] instanceof File)) {
-            setRealod(realod+1)
+            setRealod(realod + 1)
         }
 
     }, [form?.values[field?.name]]);
 
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
 
         const files = e.target.files;
-        let myFiles = Array.from(files)
-        myFiles = myFiles[0]
+        const myFiles = Array.from(files)
+
         // Use the name attribute of the field to update its value
         // form.setFieldValue(field.name, myFiles.length > 0 ? myFiles : '')
-        form.setFieldValue(field.name, myFiles)
+        form.setFieldValue(field.name, myFiles[0])
     }
 
     return <input key={realod} type="file" {...rest} onChange={handleChange}/>;
