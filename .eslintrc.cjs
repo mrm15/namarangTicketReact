c39cdff6bed458bd86d59d58713cb85a5f446af4
mrm15 <mrm.learn@gmail.com>
2024-03-03 12:40:09 +0330

    skip type errors please!

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,25 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    //  dont check unsafe please
+    // Disable all TypeScript-related rules
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/restrict-template-expressions' : 'off',
+    '@typescript-eslint/no-unsafe-member-access' : 'off',
+    '@typescript-eslint/no-unsafe-assignment' : 'off',
+    '@typescript-eslint/ban-ts-comment' : 'off',
+    '@typescript-eslint/no-unsafe-return' : 'off',
+    '@typescript-eslint/no-unsafe-call' : 'off',
+    '@typescript-eslint/restrict-plus-operands' : 'off',
+    '@typescript-eslint/no-unsafe-argument' : 'off',
+    '@typescript-eslint/no-misused-promises' : 'off',
+    // Disable all other rules if you want to write code as in JavaScript
+    'no-restricted-globals': 'off',
+    // Add more rules here as needed
+    /////////////////////////////
+
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
