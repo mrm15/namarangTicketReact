1bee2e2969959e851426b034839ee995bec303cd
mrm15 <mrm.learn@gmail.com>
2024-03-09 09:23:35 +0330

    Update Route-string.tsx

--- src/Pages/Route-string.tsx
+++ src/Pages/Route-string.tsx
@@ -1,35 +1,38 @@
-
-export const PAGES= {
+export const PAGES = {
     LOGIN: '/login',
     DASHBOARD: '/',
 
-    USER_ADD_EDIT:'/add-edit-user',
-    USER_LIST:'/user-list',
+    USER_ADD_EDIT: '/add-edit-user',
+    USER_LIST: '/user-list',
 
-    ROLE_ADD_EDIT:'/role-add-edit',
-    ROLE_LIST:'/role-list',
+    ROLE_ADD_EDIT: '/role-add-edit',
+    ROLE_LIST: '/role-list',
 
-    DEPARTMENT_ADD_EDIT:'/department-add-edit',
-    DEPARTMENT_LIST:'/department-list',
+    DEPARTMENT_ADD_EDIT: '/department-add-edit',
+    DEPARTMENT_LIST: '/department-list',
 
-    STATUS_ADD_EDIT:'/status-add-edit',
-    STATUS_LIST:'/status-list',
+    STATUS_ADD_EDIT: '/status-add-edit',
+    STATUS_LIST: '/status-list',
 
 
-    FILE_ADD_EDIT:'/file-add-edit',
-    FILE_LIST:'/file-list',
+    FILE_ADD_EDIT: '/file-add-edit',
+    FILE_LIST: '/file-list',
 
-    ticket_Create:'/ticket-new', // ad new ticket
-    ticket_Read_All:'/ticket-list', // show list Of all tickets
-    ticketInput:'/ticket-list', // show list Of tickets
-    ticket_r:'/inboxList', // show list Of tickets
-    ticket_own_sent:'/ticket-sent', // show list Of tickets
+    ticket_Create: '/ticket-new', // ad new ticket
+    ticket_own_sent: '/ticket-sent', // show list Of sent Tickets
+    // all tickets for Admin
+    ticket_Read_All: '/ticket-list', // show list Of all tickets just admin
+    // my inbox    in department and in my access list
+    ticketInbox: '/ticket-inbox', // show list Of tickets in my department and in my access list
+    // all my access list ticket
+    ticket_read_my_all_tickets: '/readMyAllTickets', // show list Of tickets
+    // sent Ticket Link
+    /////////////////////////////////////////
 
-    ticket_chat_list:'/ticket-chat-list', // ad new ticket
-    Tickets_Incoming:'IncomingTickets',
-    ticket_Read_Own:'/ticket-read-own', // read ticket with ticket replies
-    ticket_Update:'/ticket-new', // change ticket text file description ( just Admin)
-
+    ticket_chat_list: '/ticket-chat-list', // ad new ticket
+    Tickets_Incoming: 'IncomingTickets',
+    ticket_Read_Own: '/ticket-read-own', // read ticket with ticket replies
+    ticket_Update: '/ticket-new', // change ticket text file description ( just Admin)
 
 
 };
