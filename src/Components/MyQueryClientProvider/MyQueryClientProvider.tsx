90cdddb6f27ff4066ab9aea09bda3e9a265dc09e
mrm15 <mrm.learn@gmail.com>
2024-03-17 13:14:15 +0330

    Create MyQueryClientProvider.tsx

---
+++ src/Components/MyQueryClientProvider/MyQueryClientProvider.tsx
@@ -0,0 +1,19 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+} from '@tanstack/react-query'
+
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient()
+
+export default function MyQueryClientProvider({children}) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
+}
