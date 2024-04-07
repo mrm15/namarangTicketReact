d35b25593c4b67d1a8053e68fd0df1b3088cc306
mrm15 <mrm.learn@gmail.com>
2024-04-07 18:00:43 +0330

    Create UsualUserViewSection.tsx

---
+++ src/Components/Ticket/ForwardModal/UsualUserViewSection.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import DepartmentList from "./DepartmentList.tsx";
+
+const UsualUserViewSection = ({mode, setSelectedData, destinationUserList}) => {
+    return (
+        <div>
+            <div>
+
+                <div className={' flex gap-2'}>
+                    <div className={'w-full'}>
+                        <div> انتخاب کاربر</div>
+                        <DepartmentList
+                            myOptions={destinationUserList}
+                            myKey={'user'}
+                            setSelectedData={setSelectedData}
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default UsualUserViewSection;
