6ceac75d6715a0382fef030b690d30bf4ee1c31d
mrm15 <mrm.learn@gmail.com>
2024-04-17 09:54:55 +0330

    Modal show Buttons

--- src/Components/Modal/ModalView.tsx
+++ src/Components/Modal/ModalView.tsx
@@ -12,16 +12,16 @@ const MyComponent = (props) => {
             <div className={'mt-4  p-3 border-b-2 '}>
                 {props.children}
             </div>
-            <div className={'mt-4  p-3 flex gap-5 justify-center'}>
-                <button
-                    className={'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded'}
-                    onClick={props.closeModal}>لغو
-                </button>
-                <button
-                    className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'}
-                    onClick={props.onSubmit}>تایید
-                </button>
-            </div>
+            {props.showButtons !== false && <div className={'mt-4  p-3 flex gap-5 justify-center'}>
+              <button
+                className={'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded'}
+                onClick={props.closeModal}>لغو
+              </button>
+              <button
+                className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'}
+                onClick={props.onSubmit}>تایید
+              </button>
+            </div>}
 
 
         </div>
