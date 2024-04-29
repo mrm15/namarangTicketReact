3b36e7415d246ec5b16933a7cf162b90d7d3f6cb
mrm15 <mrm.learn@gmail.com>
2024-04-29 14:28:54 +0330

    Update Unauthorized.tsx

--- src/Components/Unauthorized/Unauthorized.tsx
+++ src/Components/Unauthorized/Unauthorized.tsx
@@ -1,6 +1,28 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useCallback, useEffect, useState} from "react";
+import notFoundPhoto from "../../assets/images/403-error.png"
 
 const Unauthorized = () => {
+
+    const [counter, setCounter] = useState(4)
+    const navigateTo = useNavigate();
+
+    const handleFunction = useCallback(() => {
+        if (counter < 1) {
+            navigateTo(-1);
+        } else {
+            setCounter(ps => ps - 1);
+        }
+
+    }, [counter]);
+
+    useEffect(() => {
+        const temp = setInterval(handleFunction, 1000);
+        return () => {
+            clearInterval(temp)
+        }
+    }, [handleFunction]);
+
 
     return (
         <div>
@@ -8,18 +30,27 @@ const Unauthorized = () => {
                 <div className={'p-5 leading-6'}>
                     کاربر عزیز
                     <br/>
-                    همه چی سرجاش هست و شما هنوزم میتونید با سامانه کار کنید فقط دسترسی به این صفحه رو ندارید. همین!
+                    شما مجوز دسترسی به این صفحه رو ندارید!
 
                 </div>
-                <Link to={"/"}
-                className={'cursor-pointer bg-blue-400 p-2 rounded' +
-                    ' ' +
-                    'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'}
+                <div><Link to={"/"}
+                           className={'cursor-pointer bg-blue-400 p-2 rounded' +
+                               ' ltr  ' +
+                               'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'}
                 >
+
+
                     برو به داشبورد
-                </Link>
+
+                    ({counter})
+
+                </Link></div>
+                <br/>
+                <img className="w-full h-auto" src={notFoundPhoto} alt="یافت نشد"/>
             </div>
+
         </div>
+
     );
 };
 
