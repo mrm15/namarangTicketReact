0ded579eca021351f94672d8e11514a6b0c3c5f3
mrm15 <mrm.learn@gmail.com>
2024-05-18 11:20:47 +0330

    Update MyQueryClientProvider.tsx

--- src/Components/MyQueryClientProvider/MyQueryClientProvider.tsx
+++ src/Components/MyQueryClientProvider/MyQueryClientProvider.tsx
@@ -13,7 +13,7 @@ export default function MyQueryClientProvider({children}) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/*<ReactQueryDevtools initialIsOpen={false} />*/}
         </QueryClientProvider>
     )
 }
