eb9b99e0c816513e5dbce56c6a65882797f83cf1
mrm15 <mrm.learn@gmail.com>
2024-05-18 11:20:34 +0330

    some change in missing Page

--- src/Components/Missing.tsx
+++ src/Components/Missing.tsx
@@ -1,14 +1,46 @@
-import { Link } from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
+import {useCallback, useEffect, useState} from "react";
 
 const Missing = () => {
+
+    const [counter, setCounter] = useState(15)
+    const navigateTo = useNavigate();
+
+    const handleFunction = useCallback(() => {
+        if (counter < 1) {
+            navigateTo(-1);
+        } else {
+            setCounter(ps => ps - 1);
+        }
+
+    }, [counter]);
+
+    useEffect(() => {
+        const temp = setInterval(handleFunction, 1000);
+        return () => {
+            clearInterval(temp)
+        }
+    }, [handleFunction]);
+
+
     return (
-        <article style={{ padding: "100px" }}>
-            <h1>صفحه ی مورد نظر  وجود نداره</h1>
-            <p>Page Not Found</p>
-            <div className="flexGrow cursor-pointer text-blue-800">
-                <Link to="/">بریم ب صفحه ی اصلی </Link>
-            </div>
-        </article>
+        <div
+        className={'w-full min-h-screen bg-fuchsia-300 flex justify-center align-middle'}
+        >
+            <article style={{padding: "100px"}}
+                     className={''}
+            >
+                <h1>صفحه ی مورد نظر وجود نداره
+
+                </h1>
+                <p className={'p-5 ltr'}>Page Not Found</p>
+                <div className="btn-submit-mir w-fit">
+                    <Link to="/">رفتن به صفحه ی اصلی
+                        <span>({counter})</span>
+                    </Link>
+                </div>
+            </article>
+        </div>
     )
 }
 
