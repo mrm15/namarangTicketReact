32f29e535cb256e369deaea9390c2d080d30cb6e
mrm15 <mrm.learn@gmail.com>
2024-06-26 11:27:14 +0330

    add bill table

--- src/Components/Hesabfa/ShowBill/BillTable.tsx
+++ src/Components/Hesabfa/ShowBill/BillTable.tsx
@@ -1,64 +1,78 @@
 import React from 'react';
-import {formattedNumber, timestampToTime, timestampToTimeFromHesabfa} from "../../../utils/utilsFunction.tsx";
+import {formattedNumber, timestampToTimeFromHesabfa} from "../../../utils/utilsFunction.tsx";
+import "./billTable.scss"
 
 const BillTable = ({hesabfaBillData}) => {
 
     let totalSumShow = 0
+    let sumOfNumbers = 0
 
+    const dateShow = timestampToTimeFromHesabfa(hesabfaBillData?.Date)?.split(',')[0]
+    const dueDateShow = timestampToTimeFromHesabfa(hesabfaBillData?.DueDate)?.split(',')[0]
     try {
-        return <div>
+        return <div className={'bill__table__css'}>
+            <div className={'flex flex-wrap my-3 justify-between bill__info'}>
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}>عنوان مشتری</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.ContactTitle}</div>
+                </div>
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> شماره فاکتور</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.Number}</div>
+                </div>
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> تاریخ</div>
+                    <div className={'bill__info__item__description'}>{dateShow}</div>
+                </div>
 
-            <div className={'flex flex-wrap gap-6 my-3 mx-6'}>
-                <div> شماره فاکتور:
-                    {hesabfaBillData?.Number}
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> تاریخ سر رسید</div>
+                    <div className={'bill__info__item__description'}>{dueDateShow}</div>
                 </div>
-                <div> تاریخ:
-                    {timestampToTimeFromHesabfa(hesabfaBillData?.Date)}
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> کد مشتری</div>
+                    <div className={'bill__info__item__description'}>{Number(hesabfaBillData?.ContactCode)}</div>
                 </div>
-                <div> تاریخ سر رسید:
-                    {timestampToTimeFromHesabfa(hesabfaBillData?.DueDate)}
+
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> نام مشتری</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.Contact?.Name}</div>
                 </div>
-                <div> کد مشتری:
-                    {Number(hesabfaBillData?.ContactCode)}
+
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> استان</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.Contact?.State}</div>
                 </div>
-                <div> نام مشتری:
-                    {hesabfaBillData?.Contact?.Name}
+
+
+                {/*<div> شهر مشتری:*/}
+                {/*    {hesabfaBillData?.Contact?.City}*/}
+                {/*</div>*/}
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> عنوان پروژه</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.Project}</div>
                 </div>
-                <div> نام مشتری:
-                    {hesabfaBillData?.Contact?.Name}
-                </div>
-                <div> استان مشتری:
-                    {hesabfaBillData?.Contact?.State}
-                </div>
-                <div> شهر مشتری:
-                    {hesabfaBillData?.Contact?.City}
-                </div>
-                <div> عنوان مشتری:
-                    {hesabfaBillData?.ContactTitle}
-                </div>
-                <div> عنوان پروژه:
-                    {hesabfaBillData?.Project}
-                </div>
-                <div> برچسب:
-                    {hesabfaBillData?.Tag}
+                <div className={'bill__info__item'}>
+                    <div className={'bill__info__item_title'}> برچسب</div>
+                    <div className={'bill__info__item__description'}>{hesabfaBillData?.Tag}</div>
                 </div>
             </div>
             <div>
                 <div className="overflow-x-auto">
-                    <table className="table-auto border-collapse border border-gray-200">
+                    <table className="table-auto border-collapse border border-gray-200 w-full fontSize14">
                         <thead>
-                        <tr className="bg-gray-100">
-                            <th className="border border-gray-300 px-4 py-2">ردیف</th>
-                            <th className="border border-gray-300 px-4 py-2">توضیح</th>
-                            <th className="border border-gray-300 px-4 py-2">مقدار</th>
-                            <th className="border border-gray-300 px-4 py-2">واحد</th>
-                            <th className="border border-gray-300 px-4 py-2">قیمت واحد</th>
-                            <th className="border border-gray-300 px-4 py-2">جمع</th>
-                            <th className="border border-gray-300 px-4 py-2">جمع کل</th>
+                        <tr className="">
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">ردیف</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">توضیح</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">مقدار</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">واحد</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">قیمت واحد</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">جمع</th>
+                            <th className="border border-b-2 border-gray-300 px-4 py-2">جمع کل</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {hesabfaBillData?.InvoiceItems.map(row => {
+                        {hesabfaBillData?.InvoiceItems.map((row, index) => {
                             const {
                                 RowNumber,
                                 Description,
@@ -68,8 +82,10 @@ const BillTable = ({hesabfaBillData}) => {
                                 Sum,
                                 TotalAmount,
                             } = row;
+                            const isEven = (index % 2 === 0)
                             totalSumShow += TotalAmount
-                            return <tr className="bg-gray-100">
+                            sumOfNumbers += Quantity
+                            return <tr className={isEven ? 'bg-gray-100' : 'bg-white'}>
                                 <td className="border border-gray-300 px-4 py-2 text-center">{RowNumber + 1}</td>
                                 <td className="border border-gray-300 px-4 py-2">{Description}</td>
                                 <td className="border border-gray-300 px-4 py-2">{Quantity}</td>
@@ -79,13 +95,31 @@ const BillTable = ({hesabfaBillData}) => {
                                 <td className="border border-gray-300 px-4 py-2">{formattedNumber(TotalAmount)}</td>
                             </tr>
                         })}
+
+                        <tr className={'font-bold'}>
+                            <td className="border border-t-2 border-gray-300 px-4 py-2 text-center">جمع</td>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2"></th>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2">{sumOfNumbers}</th>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2"></th>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2"> </th>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2"></th>
+                            <th className="border border-t-2 border-gray-300 px-4 py-2 text-right">
+                                {formattedNumber(totalSumShow)}
+                                &nbsp;
+                                تومان
+                            </th>
+                        </tr>
                         </tbody>
+
                     </table>
 
-                    <div className={'w-full text-center p-3'}>
-                        جمع کل فاکتور:
-                        {formattedNumber(totalSumShow)}
-                    </div>
+                    {/*<div className={'w-full text-center p-3'}>*/}
+                    {/*    جمع کل فاکتور:*/}
+                    {/*    &nbsp;*/}
+                    {/*    {formattedNumber(totalSumShow)}*/}
+                    {/*    &nbsp;*/}
+                    {/*    تومان*/}
+                    {/*</div>*/}
                 </div>
             </div>
 
--- src/Components/Hesabfa/ShowBill/ShowBill.tsx
+++ src/Components/Hesabfa/ShowBill/ShowBill.tsx
@@ -64,24 +64,28 @@ const ShowBill = () => {
         return (
             <>
                 {isLoading ? <Loader/> :
-                    <div>
+                    <div className={'m-3'}>
 
-                        <div className={"bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"}>
-                            اینجا قراره یه فاکتور با استایل جدید نمایش داده بشه.
-                            <hr/>
-                            معرکه نیست؟
-                            <hr/>
-                            خلق یک اثر جدید
-                            <hr/>
+                        <div className={'font-bold text-center '}>
+                            کارخانه حروف سازی نمارنگ
                         </div>
-                        <pre dir={'ltr'}>
-                              ✅      hasAccessToDownloadPdf is {hasAccessToDownloadPdf ? "Active" : "inActive"}
-                            <hr/>
-                              ✅      hasAccessToDownloadCSV is {hasAccessToDownloadCSV ? "Active" : "inActive"}
-                            <hr/>
-                              ✅      orderCode is {orderCode || 'کد سفارش مشخص نیست'}
-                            <hr/>
-                        </pre>
+
+                        {/*<div className={"bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"}>*/}
+                        {/*    اینجا قراره یه فاکتور با استایل جدید نمایش داده بشه.*/}
+                        {/*    <hr/>*/}
+                        {/*    معرکه نیست؟*/}
+                        {/*    <hr/>*/}
+                        {/*    خلق یک اثر جدید*/}
+                        {/*    <hr/>*/}
+                        {/*</div>*/}
+                        {/*<pre dir={'ltr'}>*/}
+                        {/*      ✅      hasAccessToDownloadPdf is {hasAccessToDownloadPdf ? "Active" : "inActive"}*/}
+                        {/*    <hr/>*/}
+                        {/*      ✅      hasAccessToDownloadCSV is {hasAccessToDownloadCSV ? "Active" : "inActive"}*/}
+                        {/*    <hr/>*/}
+                        {/*      ✅      orderCode is {orderCode || 'کد سفارش مشخص نیست'}*/}
+                        {/*    <hr/>*/}
+                        {/*</pre>*/}
                         <div>
                             <BillTable hesabfaBillData={hesabfaBillData}/>
                         </div>
