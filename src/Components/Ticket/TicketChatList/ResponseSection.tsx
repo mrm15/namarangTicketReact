010242e31ebd86ef8db0f66cd209a6db2d2f9d7a
mrm15 <mrm.learn@gmail.com>
2024-07-15 17:13:06 +0330

    Update ResponseSection.tsx

--- src/Components/Ticket/TicketChatList/ResponseSection.tsx
+++ src/Components/Ticket/TicketChatList/ResponseSection.tsx
@@ -1,18 +1,17 @@
-import {FaTrash} from "react-icons/fa";
-import React, {useEffect, useRef, useState} from "react";
-import {handleDragOver, randomNumberGenerator} from "../../../utils/utilsFunction.tsx";
+import { FaTrash } from "react-icons/fa";
+import React, { useEffect, useRef, useState } from "react";
+import { handleDragOver, randomNumberGenerator } from "../../../utils/utilsFunction.tsx";
 import useAxiosPrivateFormData from "../../../hooks/useAxiosPrivateFormData.tsx";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate.tsx";
-import {AxiosResponse} from "axios";
-import {uploadFileUtil} from "../../../utils/upload.tsx";
-import {toast} from "react-toastify";
+import { AxiosResponse } from "axios";
+import { uploadFileUtil } from "../../../utils/upload.tsx";
+import { toast } from "react-toastify";
 import useObjectDataHolder from "../../../hooks/UseObjectDataHolder.tsx";
-import {RiAttachmentLine} from "react-icons/ri";
-import {FiPaperclip} from "react-icons/fi";
+import { FiPaperclip } from "react-icons/fi";
 import useAuth from "../../../hooks/useAuth.tsx";
-import {ROLES} from "../../../Pages/ROLES.tsx";
-import {PAGES} from "../../../Pages/Route-string.tsx";
-import {useNavigate} from "react-router-dom";
+import { ROLES } from "../../../Pages/ROLES.tsx";
+import { PAGES } from "../../../Pages/Route-string.tsx";
+import { useNavigate } from "react-router-dom";
 
 const requestUrl = '/ticketReply/create';
 
@@ -22,36 +21,57 @@ interface IInitialSendData {
     attachments: File[] | [];
 }
 
-const ResponseSection = ({chatList, setReload, reload}) => {
+const ResponseSection = ({ chatList, setReload, reload }) => {
     const initialSendData: IInitialSendData = {
         description: '',
         visibleToUser: true,
         attachments: []
     }
     const navigateTo = useNavigate()
-
     const messagesEndRef = useRef(null);
-    // Function to scroll to the bottom of the container
+    const [isHideCheckBox, setIsHideCheckBox] = useState(false)
+    const [sendData, setSendData] = useObjectDataHolder({ ...initialSendData })
+    const fileInputRef = useRef(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const myAxiosPrivate = useAxiosPrivate()
+    const myAxiosPrivateFormData = useAxiosPrivateFormData()
+
     const scrollToBottom = () => {
         if (messagesEndRef.current) {
             setTimeout(() => {
-                messagesEndRef.current.scrollIntoView({behavior: 'smooth'});
+                messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
             }, 500)
         }
     };
-    // Scroll to the bottom of the container when messages update
+
     useEffect(() => {
         scrollToBottom();
     }, [reload]);
 
+    useEffect(() => {
+        const handlePaste = (event: ClipboardEvent) => {
+            if (event.clipboardData && event.clipboardData.files.length > 0) {
+                const pastedFiles = Array.from(event.clipboardData.files).filter(file => file.type.startsWith('image/'));
+                if (pastedFiles.length > 0) {
+                    toast.info("فایل ضمیمه شد.")
+                    setSendData({ attachments: [...sendData.attachments, ...pastedFiles] });
+                }
+            }
+        };
 
-    const [isHideCheckBox, setIsHideCheckBox] = useState(false)
-    const [sendData, setSendData] = useObjectDataHolder({...initialSendData})
-    const fileInputRef = useRef(null);
-    const myAxiosPrivate = useAxiosPrivate()
-    const myAxiosPrivateFormData = useAxiosPrivateFormData()
+        const textarea = textareaRef.current;
+        if (textarea) {
+            textarea.addEventListener('paste', handlePaste);
+        }
+
+        return () => {
+            if (textarea) {
+                textarea.removeEventListener('paste', handlePaste);
+            }
+        };
+    }, [sendData.attachments]);
+
     const submitHandler = async (inputNumber: 0 | 1) => {
-
         if (sendData.description === '') {
             toast.error('مقدار توضیحات نباید خالی باشد');
             return
@@ -82,7 +102,6 @@ const ResponseSection = ({chatList, setReload, reload}) => {
                     } catch (error) {
                         console.log(error)
                     }
-
                 }
                 toast.dismiss(tId)
             }
@@ -99,9 +118,6 @@ const ResponseSection = ({chatList, setReload, reload}) => {
 
         try {
             const response1 = await myAxiosPrivate.post(requestUrl, temp);
-
-
-            // یعنی شما صرفا ثبت سفارش کردید و قرار نیست فاکتور بزنید
             if (response1.data && inputNumber === 0) {
                 toast.success(response1.data?.message)
                 setSendData({...initialSendData})
@@ -120,16 +136,15 @@ const ResponseSection = ({chatList, setReload, reload}) => {
                                 backUrl: PAGES.ticket_own_sent,
                             }
                         }
-                    })
+                    });
                 } catch (error) {
-                    console.log(error.toString())
+                    console.log(error.toString());
                     toast.info('امکان صدور فاکتور وجود ندارد');
                 }
             }
         } catch (error) {
-            toast.error(error.toString())
+            toast.error(error.toString());
         }
-
     }
 
     const handleDrop = (event: React.DragEvent<HTMLDivElement>, index: number) => {
@@ -144,7 +159,6 @@ const ResponseSection = ({chatList, setReload, reload}) => {
     };
     const handleInputFile = (e) => {
         const files = e.target.files;
-
         if (files.length > 3) {
             toast.info('فقط سه فایل در هر تیکت میتوانید ارسال کنید')
             return
@@ -167,98 +181,88 @@ const ResponseSection = ({chatList, setReload, reload}) => {
     const sendHiddenMessage = auth?.userInfo?.roleAccessList?.includes('sendHiddenMessage');
     const hasAccessToSubmitFactorInChatList = auth?.userInfo?.roleAccessList?.includes(ROLES.submitBillInChatList[0])
     try {
-
-
         const ContentText = sendData.attachments.length > 0 ? sendData.attachments.map((file: File) => file.name).join(', ') : ` محل قرار گیری فایل`
-        return <div
-
-            className={'border-2 my-2'}>
+        return (
+            <div className={'border-2 my-2'}>
             <textarea
-
+                ref={textareaRef}
                 value={sendData.description}
-                onChange={(e) => setSendData({...sendData, description: e.target.value})}
+                onChange={(e) => setSendData({ ...sendData, description: e.target.value })}
                 className={'p-2 w-full border__gray'}
-                placeholder={'متن پاسخ را وارد کنید'}/>
+                placeholder={'متن پاسخ را وارد کنید'}
+            />
 
-            <div className={'flex justify-between'}>
-                <div className={'flex justify-center'}>
-                    <div className={'flex items-center'}>
-                        {sendData.attachments.length > 0 && <FaTrash
-                          onClick={handleRemoveFile}
-                          className={'text-red-600 ms-2 '}/>}
-                    </div>
+                <div className={'flex justify-between'}>
+                    <div className={'flex justify-center btn-gay-mir'}>
+                        <div className={'flex items-center'}>
+                            {sendData.attachments.length > 0 && <FaTrash onClick={handleRemoveFile} className={'text-red-600 ms-2 '} />}
+                        </div>
+                        <div className={'flex items-center'}><FiPaperclip /></div>
+                        <input
+                            ref={fileInputRef}
+                            multiple={true}
+                            onChange={handleInputFile}
 
-                    <div
-                        className={'flex items-center'}
-                    ><FiPaperclip/></div>
-                    <input
-                        ref={fileInputRef}
-                        multiple={true}
-                        onChange={handleInputFile}
-                        id={`file${index + 1}`} type="file" className="w-100 rounded border-2 hidden"/>
-
-                    <div className={'flex items-center'}>
-                        <label htmlFor={`file${index + 1}`}
-                               className={'customFileLabel cursor-pointer w-full'}
-                        >
-                            <div
-                                id={`file${index + 1}`}
-                                className="same__input w-full"
-                                onDrop={e => handleDrop(e, index)}
-                                onDragOver={handleDragOver}>
-                                <div className={'break-all px-1'}>
-                                    {ContentText}
+                            id={`file${index + 1}`} type="file" className="w-100 rounded border-2 hidden"
+                        />
+                        <div className={'flex items-center '}>
+                            <label htmlFor={`file${index + 1}`} className={'customFileLabel cursor-pointer w-full'}>
+                                <div
+                                    id={`file${index + 1}`}
+                                    className="same__input w-full"
+                                    onDrop={e => handleDrop(e, index)}
+                                    onDragOver={handleDragOver}
+                                >
+                                    <div className={'break-all px-1 '}>
+                                        {ContentText}
+                                    </div>
                                 </div>
-                            </div>
-                        </label>
-
+                            </label>
+                        </div>
                     </div>
-                </div>
-
-                <div className={'flex flex-col justify-center items-end'}>
-                    <div className={'flex gap-2'}>
-                        <button
-                            ref={messagesEndRef}
-                            onClick={() => submitHandler(0)}
-                            onContextMenu={(event) => {
-                                if (sendHiddenMessage) {
-                                    event.preventDefault();
-                                    setIsHideCheckBox(!isHideCheckBox)
-                                }
-                            }}
-                            className={'btn-submit-mir'}> ارسال
-                        </button>
-                        {
-                            hasAccessToSubmitFactorInChatList && <>
-
+                    <div className={'flex flex-col justify-center items-end'}>
+                        <div className={'flex gap-2'}>
                             <button
-                              ref={messagesEndRef}
-                              onClick={() => submitHandler(1)}
-                              onContextMenu={(event) => {
-                                  if (sendHiddenMessage) {
-                                      event.preventDefault();
-                                      setIsHideCheckBox(!isHideCheckBox)
-                                  }
-                              }}
-                              className={'btn-submit-mir'}> ارسال و ثبت فاکتور
+                                ref={messagesEndRef}
+                                onClick={() => submitHandler(0)}
+                                onContextMenu={(event) => {
+                                    if (sendHiddenMessage) {
+                                        event.preventDefault();
+                                        setIsHideCheckBox(!isHideCheckBox)
+                                    }
+                                }}
+                                className={'btn-submit-mir'}> ارسال
                             </button>
-
-                          </>
-                        }
+                            {hasAccessToSubmitFactorInChatList && (
+                                <button
+                                    ref={messagesEndRef}
+                                    onClick={() => submitHandler(1)}
+                                    onContextMenu={(event) => {
+                                        if (sendHiddenMessage) {
+                                            event.preventDefault();
+                                            setIsHideCheckBox(!isHideCheckBox)
+                                        }
+                                    }}
+                                    className={'btn-submit-mir'}> ارسال و ثبت فاکتور
+                                </button>
+                            )}
+                        </div>
+                        {isHideCheckBox && (
+                            <div className={'border border-blue-200 rounded m-2 p-2'}>
+                                <input checked={!sendData.visibleToUser}
+                                       onChange={(e) => {
+                                           const checked = e.target.checked
+                                           setSendData({ visibleToUser: !checked })
+                                       }}
+                                       id={'hideMessageFromUser'} type={'checkbox'}
+                                />
+                                <label htmlFor="hideMessageFromUser" className={'fontSize10'}> ارسال پیام در حالت ویسپر</label>
+                            </div>
+                        )}
                     </div>
-
-                    {isHideCheckBox && <div className={'border border-blue-200 rounded m-2 p-2'}>
-                      <input checked={!sendData.visibleToUser}
-                             onChange={(e) => {
-                                 const checked = e.target.checked
-                                 setSendData({visibleToUser: !checked})
-                             }}
-                             id={'hideMessageFromUser'} type={'checkbox'}/>
-                      <label htmlFor="hideMessageFromUser" className={'fontSize10'}> ارسال پیام در حالت ویسپر</label>
-                    </div>}
                 </div>
             </div>
-        </div>
+        );
     } catch (error) {
         return <div>{error.toString()}</div>
     }
