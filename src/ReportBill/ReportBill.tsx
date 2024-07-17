86c33f419a3aaa6497fff61196a5c8ff63e7c708
mrm15 <mrm.learn@gmail.com>
2024-07-17 15:40:29 +0330

    Create ReportBill.tsx

---
+++ src/ReportBill/ReportBill.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ReportBill = () => {
+
+
+    return (
+        <div>
+            <div className={'text-center bg-gray-300 block rounded p-5 m-1'}> خروجی اکسل از فایل ها</div>
+
+
+        </div>
+    );
+};
+
+export default ReportBill;
