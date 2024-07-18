2b9813c2b85c277a17c1dfb0939d498df13d965b
mrm15 <mrm.learn@gmail.com>
2024-07-19 02:02:25 +0330

    Create excelExport.tsx

---
+++ src/utils/excelExport.tsx
@@ -0,0 +1,27 @@
+import * as XLSX from 'xlsx';
+import {saveAs} from 'file-saver';
+
+export const excelExport = ({data, headers, fileName}) => {
+    // Format the data with headers
+    const formattedData = data.map((item) => {
+        const newItem: { [key: string]: any } = {};
+        headers.forEach((header) => {
+            newItem[header.title] = header.task ? header.task((item)[header.key]) : (item)[header.key];
+        });
+        return newItem;
+    });
+
+    // Create a worksheet
+    const worksheet = XLSX.utils.json_to_sheet(formattedData);
+
+    // Create a new workbook and append the worksheet
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    // Write the workbook and create a Blob
+    const excelBuffer = XLSX.write(workbook, {bookType: 'xlsx', type: 'array'});
+    const dataBlob = new Blob([excelBuffer], {type: 'application/octet-stream'});
+
+    // Use file-saver to save the file
+    saveAs(dataBlob, `${fileName}.xlsx`);
+};
