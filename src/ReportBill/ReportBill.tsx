c3e6d052abc28924a5c7261e89c117f40bd2ed50
mrm15 <mrm.learn@gmail.com>
2024-07-20 17:05:52 +0330

    Update ReportBill.tsx

--- src/ReportBill/ReportBill.tsx
+++ src/ReportBill/ReportBill.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import useAxiosPrivate from "../hooks/useAxiosPrivate.tsx";
 import {getBillList} from "../config/api.tsx";
 import {timestampToFormattedDateToSendHesabfa, timestampToTimeFromHesabfa} from "../utils/utilsFunction.tsx";
-import {excelExport} from "../utils/excelExport.tsx";
+import {excelExport, excelExportForHesabfa} from "../utils/excelExport.tsx";
 import {toast} from "react-toastify";
 import {p2e} from "../utils/NumericFunction.tsx";
 
@@ -38,26 +38,105 @@ const ReportBill = () => {
         const resultOfGetFactorList = await myAxios.post(getBillList, data);
         toast.dismiss(tId)
         const fileName = new Date() + "";
-        const normalizeData = resultOfGetFactorList.data.data.List;
+        const incomeData: [] = resultOfGetFactorList.data.data.List;
+        const normalizeData = incomeData.map((row: { [key: string]: string }) => {
+
+            try {
+                const tag = JSON.parse(row.Tag)
+                return {
+                    ...row,
+                    ticketNumber: tag.tn,
+                    seller: tag.n
+                }
+            } catch (error) {
+                return row
+            }
+        });
+
 
         const DateToEnglish = (inputText: any) => {
             let result = timestampToTimeFromHesabfa(inputText)
-            result =p2e(result);
+            result = p2e(result);
             return result
         }
+        //
 
-        const headers = [
+        const changeTextTo = (inputNumber: any) => {
+
+            return inputNumber === 0 ? "پیشنویس" : "تایید شده"
+        }
+        //
+
+        const headersSheet1 = [
+            ////////////////////////////////
             {title: "شماره فاکتور", key: "Number"},
             {title: "فروشنده", key: "Tag"},
             {title: "کد مشتری", key: "ContactCode"},
             {title: "واحد پولی", key: "Currency"},
             {title: "تاریخ ", key: "Date", task: DateToEnglish},
             {title: "سر رسید ", key: "DueDate"},
-            {title: "سر رسید ", key: "DueDate"},
             {title: "جمع کل", key: "Sum"},
-            {title: "وضعیت", key: "Status"},
+            {title: "وضعیت", key: "Status", task: changeTextTo},
         ]
-        excelExport({data: normalizeData, fileName, headers})
+
+        const dataSheet2 = []
+
+        incomeData.forEach((row: any) => {
+            let myTicketNumber = ""
+            let mySeller = ""
+            try {
+                myTicketNumber = JSON.parse(row?.Tag)?.tn || "";
+                mySeller = JSON.parse(row?.Tag)?.n || "";
+            } catch (error) {
+                myTicketNumber = ""
+                mySeller = ""
+            }
+
+            row?.InvoiceItems.forEach(rowItem => {
+                dataSheet2.push({
+                    ...rowItem,
+                    myItemCode: rowItem.Item.Code,
+                    myItemName: rowItem.Item.Name,
+                    myNumber: row.Number,
+                    myDate: row.Date,
+                    myStatus: row.Status,
+                    myCurrency: row.Currency,
+                    myContactCode: row.ContactCode,
+                    myContactTitle: row.ContactTitle,
+                    myTicketNumber,
+                    mySeller,
+                })
+            })
+        })
+
+
+        const headersSheet2 = [
+            {title: "شماره فاکتور", key: "myNumber"},
+            {title: "شماره سفارش", key: "myTicketNumber"},
+            {title: "فروشنده", key: "mySeller"},
+            {title: "تاریخ", key: "myDate", task: DateToEnglish},
+            {title: "کد شخص", key: "myContactCode"},
+            {title: "نام شخص", key: "myContactTitle"},
+            {title: "کد کالا", key: "myItemCode"},
+            {title: "کالا", key: "myItemName"},
+            {title: "شرح", key: "Description"},
+            {title: "واحد", key: "Unit"},
+            {title: "تعداد", key: "Quantity"},
+            {title: "مبلغ واحد", key: "UnitPrice"},
+            {title: "تخفیف", key: "Discount"},
+            {title: "مالیات", key: "Tax"},
+            {title: "مبلغ کل", key: "TotalAmount"},
+            {title: "واحد پول", key: "myCurrency"},
+        ]
+
+
+        excelExportForHesabfa({
+            fileName,
+            dataSheet1: normalizeData,
+            headersSheet1,
+            headersSheet2,
+            dataSheet2
+        })
 
 
     }
