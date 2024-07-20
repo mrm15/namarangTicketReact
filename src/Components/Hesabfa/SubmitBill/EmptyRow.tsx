36d9b56926cbf641316023a409cfdf4c930a21d9
mrm15 <mrm.learn@gmail.com>
2024-07-20 17:05:44 +0330

    Update EmptyRow.tsx

--- src/Components/Hesabfa/SubmitBill/EmptyRow.tsx
+++ src/Components/Hesabfa/SubmitBill/EmptyRow.tsx
@@ -4,10 +4,10 @@ const EmptyRow = () => {
     return (
         <div>
             <ul className={'min_height_45'}>
-                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map(r => <li>&nbsp;</li>)}
+                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((r,index) => <li key={index}>&nbsp;</li>)}
             </ul>
             <ul className={'min_height_45'}>
-                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map(r => <li>&nbsp;</li>)}
+                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((r,index) => <li key={index}>&nbsp;</li>)}
             </ul>
 
         </div>
