f862a3bdec219b41e7cd924377a80928553635e6
mrm15 <mrm.learn@gmail.com>
2024-07-24 22:55:38 +0330

    Create pivotArray.tsx

---
+++ src/ReportBill/PivotHere/pivotArray.tsx
@@ -0,0 +1,38 @@
+export  const pivotArray = [
+    {
+        filterTextForPivot: ["اجرت ساخت"] ,
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+    {
+        filterTextForPivot:["ترانس"],
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+    {
+        filterTextForPivot:["SMD"],
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+    {
+        filterTextForPivot:["لبه"],
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+    {
+        filterTextForPivot:["ورق استیل"],
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+    {
+        filterTextForPivot:["ورق استیل"],
+        myKey:"myItemName",
+        sumKey:"myTotalAmount",
+        countKey:"Quantity",
+    },
+]
