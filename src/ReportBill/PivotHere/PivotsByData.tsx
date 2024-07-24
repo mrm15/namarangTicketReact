f8b89797f876ba6567edff038b9add69eedc0038
mrm15 <mrm.learn@gmail.com>
2024-07-24 22:55:35 +0330

    Update PivotsByData.tsx

--- src/ReportBill/PivotHere/PivotsByData.tsx
+++ src/ReportBill/PivotHere/PivotsByData.tsx
@@ -2,54 +2,34 @@ import React, {useContext} from 'react';
 import {ReportBillContext} from "../ReportBillContext.tsx";
 import singlePivotData from "./singlePivotData.tsx";
 import SinglePivotData from "./singlePivotData.tsx";
+import PrintComponent from "../../Components/PrintComponent/PrintComponent.tsx";
+import {pivotArray} from "./pivotArray.tsx";
 
 const PivotsByData = () => {
 
     const t = useContext(ReportBillContext);
     const {awesomeData, setAwesomeData} = t
+
     return (
-        <div className={"flex flex-wrap gap-2"}>
-            <SinglePivotData
-                totalData={awesomeData.totalData}
-                filterTextForPivot={["اجرت ساخت"]}
-                myKey={"myItemName"}
-                sumKey={"myTotalAmount"}
-                countKey={"Quantity"}
-            />
-            <SinglePivotData
-                totalData={awesomeData.totalData}
-                filterTextForPivot={["ترانس"]}
-                myKey={"myItemName"}
-                sumKey={"myTotalAmount"}
-                countKey={"Quantity"}
+        <PrintComponent
+            printButtonLabel={"چاپ"}
+            orientation={"A4 landscape"}
 
-            />
-            <SinglePivotData
-                totalData={awesomeData.totalData}
-                filterTextForPivot={["SMD"]}
-                myKey={"myItemName"}
-                sumKey={"myTotalAmount"}
-                countKey={"Quantity"}
+        >
+            <div className={"flex flex-wrap gap-2 justify-center"}>
+                {pivotArray.map((row, index) => <SinglePivotData
+                    key={index}
+                    filterTextForPivot={row.filterTextForPivot}
+                    totalData={awesomeData.totalData}
+                    myKey={row.myKey}
+                    sumKey={row.sumKey}
+                    countKey={row.countKey}
+                />)}
 
-            />
-            <SinglePivotData
-                totalData={awesomeData.totalData}
-                filterTextForPivot={["لبه"]}
-                myKey={"myItemName"}
-                sumKey={"myTotalAmount"}
-                countKey={"Quantity"}
 
-            />
-            <SinglePivotData
-                totalData={awesomeData.totalData}
-                filterTextForPivot={["ورق استیل"]}
-                myKey={"myItemName"}
-                sumKey={"myTotalAmount"}
-                countKey={"Quantity"}
+            </div>
 
-            />
-
-        </div>
+        </PrintComponent>
     );
 };
 
