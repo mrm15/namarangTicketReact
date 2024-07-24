6444d259f99535370ae6bafae55699f094289939
mrm15 <mrm.learn@gmail.com>
2024-07-24 10:54:32 +0330

    Update myTypes.tsx

--- src/ReportBill/myTypes.tsx
+++ src/ReportBill/myTypes.tsx
@@ -1,15 +1,15 @@
 export interface IAwesomeData {
-    filterStatus:any[];
     columns:{[key:string]:any}[];
     numberOfRowsShowInTable: number;
     totalPages: number;
     tableHeaders: any[]; // Specify a more precise type if you know the exact structure
     tableData: any[]; // Specify a more precise type if you know the exact structure
     totalData: any[]; // Specify a more precise type if you know the exact structure
-    reload: string;
+    reload: string; // to fill current Page  from TotalData to Table Data
     TotalCount:number; // تعداد کل دیتاهای موجود در سرور برای صفحه بندی بکار میاد
     FilteredCount:number; // تعداد دیتای بازگشتی از سرور همین الان ن.ی توتال دیتا هست
     currentSelectedPage : 1,
+    filterItems : any[],
 
 }
 
