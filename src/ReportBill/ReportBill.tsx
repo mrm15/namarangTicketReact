1781c581bcb1a12712c0b5946d359d56bb619cf6
mrm15 <mrm.learn@gmail.com>
2024-07-24 21:11:09 +0330

    Update ReportBill.tsx

--- src/ReportBill/ReportBill.tsx
+++ src/ReportBill/ReportBill.tsx
@@ -15,149 +15,17 @@ import {IAwesomeData} from "./myTypes.tsx";
 import {columns, data as tableDataForTest} from "../Components/ReactTableDataShow/SampleTableData.tsx"
 import Num2persian from 'num2persian';
 import ToggleSwitch from "../Components/UI/ToggleSwitch/ToggleSwitch.tsx";
-import { makeRowIdBasedOnPageNumber} from "./functions.tsx";
+import {makeRowIdBasedOnPageNumber} from "./functions.tsx";
+import {BsFileEarmarkExcelFill} from "react-icons/bs";
+import {MdPivotTableChart} from "react-icons/md";
+import {HandleExportToExcel} from "./HandleExportToExcel.tsx";
+import PivotsByData from "./PivotHere/PivotsByData.tsx";
 
 
-type times = "today" | "thisMonth" | "all"
 const ReportBill = () => {
 
 
     const myAxios = useAxiosPrivate();
-    const getFactorList = async (times: times) => {
-
-        let queryInfo = {}
-
-        if (times === "today") {
-            queryInfo = {
-                SortBy: 'Date',
-                SortDesc: true,
-                Take: 500,
-                Skip: 0,
-                "filters": [
-                    {
-                        "property": "Date",
-                        "operator": "=",
-                        "value": timestampToFormattedDateToSendHesabfa(new Date())
-                    },
-                    {
-                        "property": "Status",
-                        "operator": "=",
-                        "value": "1"
-                    }
-                ]
-            }
-
-        }
-
-
-        const data = {queryInfo}
-        const tId = toast.loading("در حال تهیه اکسل")
-        const resultOfGetFactorList = await myAxios.post(getBillList, data);
-        toast.dismiss(tId)
-        const fileName = new Date() + "";
-        const incomeData: [] = resultOfGetFactorList.data.data.List;
-        const normalizeData = incomeData.map((row: { [key: string]: string }) => {
-
-            try {
-                const tag = JSON.parse(row.Tag)
-                return {
-                    ...row,
-                    ticketNumber: tag.tn,
-                    seller: tag.n
-                }
-            } catch (error) {
-                return row
-            }
-        });
-
-
-        const DateToEnglish = (inputText: any) => {
-            let result = timestampToTimeFromHesabfa(inputText)
-            result = p2e(result);
-            return result
-        }
-        //
-
-        const changeTextTo = (inputNumber: any) => {
-
-            return inputNumber === 0 ? "پیشنویس" : "تایید شده"
-        }
-        //
-
-        const headersSheet1 = [
-            ////////////////////////////////
-            {title: "شماره فاکتور", key: "Number"},
-            {title: "فروشنده", key: "Tag"},
-            {title: "کد مشتری", key: "ContactCode"},
-            {title: "واحد پولی", key: "Currency"},
-            {title: "تاریخ ", key: "Date", task: DateToEnglish},
-            {title: "سر رسید ", key: "DueDate"},
-            {title: "جمع کل", key: "Sum"},
-            {title: "وضعیت", key: "Status", task: changeTextTo},
-        ]
-
-        const dataSheet2 = []
-
-        incomeData.forEach((row: any) => {
-            let myTicketNumber = ""
-            let mySeller = ""
-            try {
-                myTicketNumber = JSON.parse(row?.Tag)?.tn || "";
-                mySeller = JSON.parse(row?.Tag)?.n || "";
-            } catch (error) {
-                myTicketNumber = ""
-                mySeller = ""
-            }
-
-            row?.InvoiceItems.forEach(rowItem => {
-                dataSheet2.push({
-                    ...rowItem,
-                    myItemCode: rowItem.Item.Code,
-                    myItemName: rowItem.Item.Name,
-                    myNumber: row.Number,
-                    myDate: row.Date,
-                    myStatus: row.Status,
-                    myCurrency: row.Currency,
-                    myContactCode: row.ContactCode,
-                    myContactTitle: row.ContactTitle,
-                    myTicketNumber,
-                    mySeller,
-                })
-            })
-        })
-
-
-        const headersSheet2 = [
-            {title: "شماره فاکتور", key: "myNumber"},
-            {title: "شماره سفارش", key: "myTicketNumber"},
-            {title: "فروشنده", key: "mySeller"},
-            {title: "تاریخ", key: "myDate", task: DateToEnglish},
-            {title: "کد شخص", key: "myContactCode"},
-            {title: "نام شخص", key: "myContactTitle"},
-            {title: "کد کالا", key: "myItemCode"},
-            {title: "کالا", key: "myItemName"},
-            {title: "شرح", key: "Description"},
-            {title: "واحد", key: "Unit"},
-            {title: "تعداد", key: "Quantity"},
-            {title: "مبلغ واحد", key: "UnitPrice"},
-            {title: "تخفیف", key: "Discount"},
-            {title: "مالیات", key: "Tax"},
-            {title: "مبلغ کل", key: "TotalAmount"},
-            {title: "واحد پول", key: "myCurrency"},
-        ]
-
-
-        excelExportForHesabfa({
-            fileName,
-            dataSheet1: normalizeData,
-            headersSheet1,
-            headersSheet2,
-            dataSheet2
-        })
-
-
-    }
-
 
 
     const [awesomeData, setAwesomeData] = useObjectDataHolder<IAwesomeData>({
@@ -171,12 +39,20 @@ const ReportBill = () => {
         TotalCount: 10000000,
         FilteredCount: 10000,
         currentSelectedPage: 1,
-        filterItems:[],
+        filterItems: [],
     });
 
     // useEffect(() => {
     //     void updateTableData();
     // }, []);
+
+    const exportToExcel = async () => {
+        await HandleExportToExcel({myAxios, awesomeData})
+    }
+    const pivotTotalData = async () => {
+        await HandleExportToExcel({myAxios, awesomeData, setAwesomeData})
+    }
+
 
     const updateTableData = async () => {
         const skipFromTheFirst = (awesomeData.currentSelectedPage - 1) * awesomeData.numberOfRowsShowInTable;
@@ -195,20 +71,15 @@ const ReportBill = () => {
         const resultOfGetFactorList = await myAxios.post(getBillList, data)
         toast.dismiss(tId)
         if (resultOfGetFactorList.status === 200) {
-            console.log("hi");
-            setAwesomeData({totalData: tableDataForTest})
-
-
             const List: [{ Sum: number }] = resultOfGetFactorList.data.data.List;
             const totalSum = List.reduce(((accumulator, currentValue) => accumulator + currentValue.Sum), 0)
             console.log("totalSum: ")
             console.log(totalSum.toLocaleString())
             console.log(Num2persian(totalSum));
-            // setAwesomeData({totalData:resultOfGetFactorList.data.data.List})
             const listOfData = resultOfGetFactorList.data.data.List
             const TotalCount = resultOfGetFactorList.data.data.TotalCount
             const FilteredCount = resultOfGetFactorList.data.data.FilteredCount
-            const temp = listOfData.map((row:any) => {
+            const temp = listOfData.map((row: any) => {
                 try {
                     const myTag = JSON.parse(row.Tag)
                     if (myTag) {
@@ -234,8 +105,6 @@ const ReportBill = () => {
             return undefined
         }
     }
-
-
 
 
     useEffect(() => {
@@ -266,8 +135,8 @@ const ReportBill = () => {
 
 
         })
-    }, [ awesomeData.numberOfRowsShowInTable, awesomeData.currentSelectedPage, awesomeData.reload,
-    awesomeData.filterItems]);
+    }, [awesomeData.numberOfRowsShowInTable, awesomeData.currentSelectedPage, awesomeData.reload,
+        awesomeData.filterItems]);
 
 
     const handleCheckToggle = (checkStatus: boolean) => {
@@ -299,30 +168,51 @@ const ReportBill = () => {
         return (
             <div>
                 <ReportBillContext.Provider value={{awesomeData, setAwesomeData}}>
-                    <ToggleSwitch checkedText={"      تایید"}
-                                  unCheckedText={"همه"}
-                                  onChange={handleCheckToggle}
-                    />
-                    <div className={'flex flex-wrap gap-2 m-5'}>
-                        <button
-                            onClick={() => getFactorList("today")}
-                            className={'btn-submit-mir'}>
-                            دریافت اکسل فاکتور های امروز
-                        </button>
-                        <button
-                            onClick={() => getFactorList("thisMonth")}
 
-                            className={'btn-gay-mir'}>
-                            دریافت اکسل فاکتور های این ماه
-                        </button>
-                        <button
-                            onClick={() => getFactorList("all")}
+                    <div className={'flex flex-wrap justify-between gap-2 m-5'}>
+                        <div>
+                            <ToggleSwitch
+                                checkedText={"      تایید"}
+                                unCheckedText={"همه"}
+                                onChange={handleCheckToggle}
+                            />
+                        </div>
+                        <div className={'flex gap-2'}>
+                            <button
+                                onClick={pivotTotalData}
+                                // className={'btn-submit-mir'}
+                                className={"bg-green-600 py-2 px-5 rounded"}
+                                title={"Pivot"}
+                            >
 
-                            className={'btn-submit-mir'}>
-                            دریافت اکسل کل فاکتورهای امسال
-                        </button>
+                                <div className={"flex items-center gap-2"}>
+                                    <MdPivotTableChart/>
+                                    {/*<div>Pivot</div>*/}
+                                </div>
+                            </button>
+
+
+                            <button
+                                onClick={exportToExcel}
+                                className={"bg-green-600 py-2 px-5 rounded"}
+                                title={"صدور به اکسل"}
+                            >
+
+                                <div className={"flex items-center gap-2"}>
+                                    <BsFileEarmarkExcelFill/>
+                                    {/*<div>Export</div>*/}
+                                </div>
+                            </button>
+                        </div>
+
                     </div>
                     <TableSection/>
+                    <div className={"mt-10"}>
+                        <hr/>
+                    </div>
+                    {awesomeData.totalData.length !== 0 && <PivotsByData/>}
+
+
                 </ReportBillContext.Provider>
             </div>
         );
