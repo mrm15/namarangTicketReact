53ad95374fa76c6d26c931b08ddf14af33280a4e
mrm15 <mrm.learn@gmail.com>
2024-07-26 20:46:41 +0330

    Update SinglePivotData.tsx

--- src/ReportBill/PivotHere/SinglePivotData.tsx
+++ src/ReportBill/PivotHere/SinglePivotData.tsx
@@ -1,57 +1,92 @@
 import React from 'react';
 import {calculatePivot} from "./pivotFunction.tsx";
-import {formatNumber} from "../../utils/utilsFunction.tsx";
+import {formatFloat, formatNumber} from "../../utils/utilsFunction.tsx";
 
-const SinglePivotData = ({filterTextForPivot , totalData, myKey , sumKey,countKey}) => {
+const SinglePivotData = ({
+                             caption,
+                             filterTextForPivot,
+                             totalData,
+                             myKey,
+                             sumKey,
+                             countKey,
+                             showSubitems = false,
+                             subRowKey = "myContactTitle",
+                             subRowValue = "myQuantity"
+                         }) => {
 
 
+    const resultArray = calculatePivot({filterTextForPivot, totalData, myKey, sumKey, countKey})
 
 
-    const resultArray = calculatePivot({filterTextForPivot , totalData, myKey , sumKey,countKey})
-
-
-    const mySum ={
-        sumKey:0,
-        countKey:0
+    const mySum = {
+        sumKey: 0,
+        countKey: 0
     };
+
+
+
 
     return (
         <div>
-            <table className={"border mt-2"}>
+            <table className={"border  "}>
                 <caption className={"font-bold border fontSize12 "}>
-                    {filterTextForPivot[0]}
+                    {caption}
                 </caption>
                 <thead>
                 <tr>
                     <th scope="col" className={"fontSize10 border "}>ردیف</th>
                     <th scope="col" className={"fontSize10 border "}>نام</th>
-                    <th scope="col" className={"fontSize10 border "}>جمع </th>
-                    <th scope="col" className={"fontSize10 border "}> تعداد </th>
+                    <th scope="col" className={"fontSize10 border "}>جمع</th>
+                    <th scope="col" className={"fontSize10 border "}> تعداد</th>
                 </tr>
                 </thead>
                 <tbody>
-                {resultArray.map((row, index)=> {
-                    mySum.sumKey +=row[sumKey]
-                    mySum.countKey +=row[countKey]
+                {resultArray.map((row: any, index) => {
+                    mySum.sumKey += row[sumKey]
+                    mySum.countKey += row[countKey]
 
 
-                    return <tr key={index}>
 
-                        <td className={' border px-1   whitespace-nowrap  fontSize10 font-medium text-gray-900'}>{index+1}</td>
-                        <td className={' border px-1   whitespace-nowrap fontSize10 font-medium text-gray-900'}>{row[myKey]}</td>
-                        <td className={' border px-1   whitespace-nowrap fontSize10 font-medium text-gray-900'}>{formatNumber(row[sumKey])}</td>
-                        <td className={' border px-1   whitespace-nowrap fontSize10 font-medium text-gray-900'}>{row[countKey].toFixed(2)}</td>
+                    if (showSubitems) {
+                        return <>
+                            <tr key={index}>
 
-                    </tr>
+                                <td className={' border px-1  whitespace-nowrap  fontSize10 font-medium text-gray-900'}>{index + 1}</td>
+                                <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{row[myKey]}</td>
+                                <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{formatNumber(row[sumKey])}</td>
+                                <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{formatFloat(row[countKey])}</td>
+                            </tr>
+                            {row.rowData.map((subRow:any,index2) => <tr  key={index2}>
+                                <td className={' border px-1 ps-2  whitespace-nowrap  fontSize8 font-medium text-gray-900'}>
+                                    <div className={"rtl"}>
+                                        {(index+1) + ". "+ (index2 + 1)}
+                                    </div>
+                                </td>
+                                <td className={' border px-1  whitespace-nowrap fontSize8 font-medium text-gray-900'}>{subRow[subRowKey]}</td>
+                                <td className={' border px-1  whitespace-nowrap fontSize8 font-medium text-gray-900'}>{formatNumber(subRow[sumKey])}</td>
+                                <td className={' border px-1  whitespace-nowrap fontSize8 font-medium text-gray-900'}>{formatFloat(subRow[subRowValue])}</td>
+                            </tr>)}
+                        </>
+                    } else {
+                        return <tr key={index}>
+
+                            <td className={' border px-1  whitespace-nowrap  fontSize10 font-medium text-gray-900'}>{index + 1}</td>
+                            <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{row[myKey]}</td>
+                            <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{formatNumber(row[sumKey])}</td>
+                            <td className={' border px-1  whitespace-nowrap fontSize10 font-medium text-gray-900'}>{formatFloat(row[countKey])}</td>
+
+                        </tr>
+                    }
+
                 })}
 
                 </tbody>
                 <tfoot>
                 <tr>
-                    <td  className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'} ></td>
-                    <td  className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'} >جمع</td>
-                    <td  className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'} >{formatNumber( mySum.sumKey)}</td>
-                    <td  className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'} >{formatNumber(mySum.countKey)}</td>
+                    <td className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'}></td>
+                    <td className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'}>جمع</td>
+                    <td className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'}>{formatNumber(mySum.sumKey)}</td>
+                    <td className={' border px-6   whitespace-nowrap fontSize10 font-bold text-gray-900'}>{formatNumber(mySum.countKey)}</td>
                 </tr>
                 </tfoot>
             </table>
