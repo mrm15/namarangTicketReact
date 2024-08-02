c4e9680a42f3315f338aa0b807e7264315d57fc5
mrm15 <mrm.learn@gmail.com>
2024-08-02 15:10:27 +0330

    update

--- src/Components/ReactTableDataShow/ReactTableDataShow.tsx
+++ src/Components/ReactTableDataShow/ReactTableDataShow.tsx
@@ -1,4 +1,3 @@
-// MyTable.tsx
 import React from 'react';
 import {
     useReactTable,
@@ -12,10 +11,10 @@ import TableFilterSection from "../../ReportBill/TableFilterSection.tsx";
 interface MyProps {
     data: { [key: string]: any }[],
     columns: ColumnDef<{ [key: string]: any }>[] // Correctly typing columns as an array of ColumnDef
-    TableFilterSection?:any
+    TableFilterSection?: any
 }
 
-const ReactTableDataShow: React.FC<MyProps> = ({data, columns , TableFilterSection}) => {
+const ReactTableDataShow: React.FC<MyProps> = ({ data, columns, TableFilterSection }) => {
     const table = useReactTable({
         data,
         columns,
@@ -27,19 +26,24 @@ const ReactTableDataShow: React.FC<MyProps> = ({data, columns , TableFilterSecti
         <div style={{
             width: myWindowsSize.widthWindowSize - 250,
             overflowX: "scroll",
-            opacity:"hidden",
-
+            opacity: "hidden",
         }}
-             className={' shadow shadow-black '}
+             className={' shadow shadow-black fontSize12'}
         >
-            <table style={{width: '100%', borderCollapse: 'collapse'}} className={'m-1'}>
+            <table style={{ width: '100%', borderCollapse: 'collapse' }} className={'m-1'}>
                 <thead>
                 {table.getHeaderGroups().map(headerGroup => (
                     <tr key={headerGroup.id}>
                         {headerGroup.headers.map(header => (
                             <th
                                 key={header.id}
-                                style={{border: '1px solid black', padding: '8px'}}
+                                style={{
+                                    border: '1px solid black',
+                                    padding: '8px',
+                                    width: header.column.getSize(), // Use column size
+                                    minWidth: header.column.columnDef.minSize, // Use column minSize
+                                    maxWidth: header.column.columnDef.maxSize, // Use column maxSize
+                                }}
                             >
                                 {flexRender(
                                     header.column.columnDef.header,
@@ -52,12 +56,18 @@ const ReactTableDataShow: React.FC<MyProps> = ({data, columns , TableFilterSecti
                 </thead>
                 <tbody>
                 {TableFilterSection && <TableFilterSection />}
-                {table.getRowModel().rows.map(row => (
+                {data.length!==0 && table.getRowModel().rows.map(row => (
                     <tr key={row.id}>
                         {row.getVisibleCells().map(cell => (
                             <td
                                 key={cell.id}
-                                style={{border: '1px solid black', padding: '8px'}}
+                                style={{
+                                    border: '1px solid black',
+                                    padding: '8px',
+                                    width: cell.column.getSize(), // Use column size
+                                    minWidth: cell.column.columnDef.minSize, // Use column minSize
+                                    maxWidth: cell.column.columnDef.maxSize, // Use column maxSize
+                                }}
                             >
                                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
                             </td>
@@ -66,6 +76,11 @@ const ReactTableDataShow: React.FC<MyProps> = ({data, columns , TableFilterSecti
                 ))}
                 </tbody>
             </table>
+            {data.length===0 && <>
+              <div className={"text-center p-5 w-full"}>
+                اطلاعاتی جهت نمایش وجود ندارد ☹️
+              </div>
+            </>}
         </div>
     );
 };
