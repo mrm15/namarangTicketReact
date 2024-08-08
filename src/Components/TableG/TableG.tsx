090afddff802e87c7848e1357edf145ff959840a
mrm15 <mrm.learn@gmail.com>
2024-08-09 00:32:31 +0330

    Update TableG.tsx

--- src/Components/TableG/TableG.tsx
+++ src/Components/TableG/TableG.tsx
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 import useObjectDataHolder from "../../hooks/UseObjectDataHolder.tsx";
-import {TableGContext} from "./TableGContext.tsx";
+import { TableGContext } from "./TableGContext.tsx";
 import FullTable from "./FullTable/FullTable.tsx";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate.tsx";
+import {IMyData} from "./myTableGTypes.tsx";
 
-const TableG = ({url = "/user/read"}) => {
-
-
-    const [myData, setMyData] = useObjectDataHolder({
-        url,
+const TableG = ({ url = "/user/read" }) => {
+    const [myData, setMyData] = useObjectDataHolder<IMyData>({
+        url: url,
         pageNumber: 1,
         currentPage: 1,
         numberOfRows: 5,
@@ -19,54 +18,57 @@ const TableG = ({url = "/user/read"}) => {
         reload: "",
         isLoading: false,
         errorMessage: "",
-    })
-
+    });
 
     const myAxios = useAxiosPrivate();
-    useEffect(() => {
-        setMyData({url})
-    }, [setMyData, url]);
-    useEffect(() => {
 
+    useEffect(() => {
+        setMyData({ url });
+    }, [setMyData, url]);
+
+    useEffect(() => {
         const updateData = async () => {
-            if (myData.isLoading) {
-                return
-            }
-            setMyData({isLoading: true})
+            if (myData.isLoading) return;
+
+            setMyData({ isLoading: true });
             const data = {
                 page: myData.pageNumber,
                 pageSize: myData.numberOfRows,
                 filters: myData.filter,
-            }
+            };
 
-            const tempMyData = {...myData}
-            const res = await myAxios.post(url, data);
-            if (res.status === 200) {
-                const responseObject = res.data
-                setMyData({
-                    // ...tempMyData,
-                    errorMessage: "",
-                    totalRows: responseObject.totalDocuments,
-                    tableData: responseObject.results,
-                    pageNumber: responseObject.currentPage,
-                    isLoading: false,
-                    // ...responseObject
-                })
-            } else {
-                setMyData({errorMessage: "Error Data Try Again!!!"})
+            try {
+                const res = await myAxios.post(url, data);
+                if (res.status === 200) {
+                    const responseObject = res.data;
+                    setMyData({
+                        errorMessage: "",
+                        totalRows: responseObject.totalDocuments,
+                        tableData: responseObject.results,
+                        pageNumber: responseObject.currentPage,
+                        isLoading: false,
+                    });
+                } else {
+                    setMyData({ errorMessage: "Error Data Try Again!!!" });
+                }
+            } catch (error) {
+                setMyData({ errorMessage: "Error Data Try Again!!!", isLoading: false });
             }
-        }
+        };
 
-        void updateData()
+        void updateData();
     }, [myAxios, myData.reload, setMyData, url]);
 
     const loaderHandle = () => {
-        setMyData({isLoading: !myData.isLoading})
-    }
+        setMyData({ isLoading: !myData.isLoading });
+    };
+
     return (
-        <TableGContext.Provider value={{myData, setMyData}}>
-            <button onClick={loaderHandle}>loader</button>
-            <FullTable/>
+        <TableGContext.Provider value={{ myData, setMyData }}>
+            <div>
+                <button onClick={loaderHandle}>loader</button>
+                <FullTable />
+            </div>
         </TableGContext.Provider>
     );
 };
