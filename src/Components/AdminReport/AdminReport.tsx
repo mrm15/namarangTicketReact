e18b239f4f6994728b0e810572c1c9393a4966e0
mrm15 <mrm.learn@gmail.com>
2024-08-20 00:22:40 +0330

    add AdminReportFilter.tsx

--- src/Components/AdminReport/AdminReport.tsx
+++ src/Components/AdminReport/AdminReport.tsx
@@ -1,8 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AdminReportContext } from './AdminReportContext.tsx';
+import React, {useContext, useEffect, useState} from 'react';
+import {AdminReportContext} from './AdminReportContext.tsx';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate.tsx';
 import SummaryReport from './SummaryReport.tsx';
 import ReportTable from './ReportTable/ReportTable.tsx';
+import AdminReportFilter from "./AdminReportFilter.tsx";
+import {formatDateForBackend} from "../../utils/utilsFunction.tsx";
 
 const AdminReport = () => {
     const url = 'reports/adminReport';
@@ -17,12 +19,15 @@ const AdminReport = () => {
     useEffect(() => {
         const getData = async () => {
             try {
-                const data = {};
+                setMyData({isLoading: true,});
+                const data = {filterItems: myData.filterItems};
                 const result = await myAxios.post(url, data);
                 const temp = result.data;
 
-                setMyData({ isLoading: false, ...temp });
+                setMyData({isLoading: false, ...temp});
             } catch (error: any) {
+                setMyData({isLoading: false,});
+
                 console.log(error);
             }
         };
@@ -31,21 +36,26 @@ const AdminReport = () => {
     }, [myData?.reload]);
 
     if (!myData || !setMyData) {
-        return <></>;
+        return <> موردی پیش اومده و اطلاعات به درستی دریافت نشد!</>;
     }
 
     return (
         <div>
-            {/*<FilterReport />*/}
-            <SummaryReport />
-            <hr />
-            <button
-                onClick={() => setShowReportTable(!showReportTable)}
-                className="  text-right text-blue-500 hover:text-blue-700 font-semibold p-5 mb-2 border-2 rounded"
-            >
-                {showReportTable ? '▲ پنهان کردن جزئیات' : '▼ مشاهده جزئیات'}
-            </button>
-            {showReportTable && <ReportTable />}
+            <AdminReportFilter/>
+            {
+                !myData.isLoading ?
+                    <>
+                        <SummaryReport/>
+                        <hr/>
+                        <button
+                            onClick={() => setShowReportTable(!showReportTable)}
+                            className="  text-right text-blue-500 hover:text-blue-700 font-semibold p-5 mb-2 border-2 rounded"
+                        >
+                            {showReportTable ? '▲ پنهان کردن جزئیات' : '▼ مشاهده جزئیات'}
+                        </button>
+                        {showReportTable && <ReportTable/>}
+                    </> : <>در حال بارگزاری...</>
+            }
         </div>
     );
 };
