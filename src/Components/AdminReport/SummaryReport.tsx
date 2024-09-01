08711948a1ba34b549113448242a1cb53e0980e4
mrm15 <mrm.learn@gmail.com>
2024-09-01 15:29:31 +0330

    remove font

--- src/Components/AdminReport/SummaryReport.tsx
+++ src/Components/AdminReport/SummaryReport.tsx
@@ -9,17 +9,16 @@ const SummaryReport = () => {
     try {
         return (
             <div className={"flex flex-wrap gap-1"}>
-                {myData?.titleData?.map((row:any, index: React.Key) => {
+                {myData?.titleData?.map((row: any, index: React.Key) => {
                     const {title, value} = row
                     return <div
                         key={index}
                         // className="bg-white shadow-lg rounded-lg p-5 m-1 transform hover:scale-105 transition-transform duration-200"
                         className="bg-white shadow-lg rounded-lg p-5 m-1 transform hover:scale-105 transition-transform duration-200 border-r-2 border-b-2 border-blue-500"
                     >
-                        <h2 className=" font-semibold text-gray-700 mb-2">{title}</h2>
-                        <p className=" font-bold text-blue-500 ltr">{value}</p>
+                        <h2 className=" text-gray-700 mb-2">{title}</h2>
+                        <p className=" text-blue-500 ltr">{value}</p>
                     </div>
-
                 })}
             </div>
         );
