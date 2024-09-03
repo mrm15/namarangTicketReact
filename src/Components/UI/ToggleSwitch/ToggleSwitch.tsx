1e75eeda5b3a7c197188022908abebc48097f5ed
mrm15 <mrm.learn@gmail.com>
2024-09-04 01:29:49 +0330

    change color

--- src/Components/UI/ToggleSwitch/ToggleSwitch.tsx
+++ src/Components/UI/ToggleSwitch/ToggleSwitch.tsx
@@ -30,7 +30,7 @@ const ToggleSwitch: React.FC<ToggleProps> = ({
                 className={`absolute top-0.5 left-0.5 block w-5 h-5 bg-white rounded-full shadow-md transform transition-transform duration-300 ${isChecked ? 'translate-x-6' : 'translate-x-0'}`}
             />
             <span
-                className={`absolute inset-0 flex items-center justify-center text-xs font-medium text-white transition-opacity duration-300 ${isChecked ? 'opacity-100' : 'opacity-0'}`}
+                className={`absolute inset-0 flex items-center justify-center text-xs font-medium  transition-opacity duration-300 text-black ${isChecked ? 'opacity-100' : 'opacity-0'}`}
             >
                 {checkedText}
             </span>
