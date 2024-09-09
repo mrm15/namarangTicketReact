b47d418ba093ec13ae1d9aef9aba4722df6363ad
mrm15 <mrm.learn@gmail.com>
2024-09-09 14:23:21 +0330

    Update TableHeader.tsx

--- src/Components/TableG/FullTable/TableShowData/TableHeader.tsx
+++ src/Components/TableG/FullTable/TableShowData/TableHeader.tsx
@@ -10,24 +10,30 @@ const TableHeader = ({table}) => {
                     className={"bg-gray-300"}
 
                 >
-                    {headerGroup.headers.map((header: any, index: React.Key) => (
-                        <th
-                            key={index}
-                            style={{
-                                padding: '5px 3px',
-                                textAlign: "center",
-                                width: header.column.getSize()+"", // Use column size
-                                minWidth: header.column.columnDef.minSize+"", // Use column minSize
-                                maxWidth: header.column.columnDef.maxSize+"", // Use column maxSize
-                            }}
-                            className={"whitespace-nowrap"}
-                        >
-                            {flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                            )}
-                        </th>
-                    ))}
+                    {headerGroup.headers.map((header: any, index: React.Key) => {
+
+                        if(header.column.columnDef.hidden){
+                            return null
+                        }
+                        return (
+                            <th
+                                key={index}
+                                style={{
+                                    padding: '5px 3px',
+                                    textAlign: "center",
+                                    width: header.column.getSize()+"", // Use column size
+                                    minWidth: header.column.columnDef.minSize+"", // Use column minSize
+                                    maxWidth: header.column.columnDef.maxSize+"", // Use column maxSize
+                                }}
+                                className={"whitespace-nowrap"}
+                            >
+                                {flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                )}
+                            </th>
+                        )
+                    })}
                 </tr>
             )
         })
