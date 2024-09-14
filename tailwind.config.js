74428e9d2cdba5daaec82aeb5952b81ce56c50e6
mrm15 <mrm.learn@gmail.com>
2024-09-14 12:26:44 +0330

    add width to tailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ export default {
         poppins: "Poppins",
         kaushan: "Kaushan Script",
       },
+      width: {
+        '640': '640px',
+        '768': '768px',
+        '1024': '1024px',
+        '1028': '1028px',
+        '1536': '1536px',
+      },
       backgroundImage: {
         hero1: "url(src/assets/images/hero1.jpg)",
         hero2: "url(src/assets/images/hero2.jpg)",
