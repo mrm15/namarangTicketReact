c07334c3f476decfabfb62f60cc1ced304c4ce0b
mrm15 <mrm.learn@gmail.com>
2024-09-25 00:14:33 +0330

    table view

--- src/Components/TableG/FullTable/TableShowData/TableBody.tsx
+++ src/Components/TableG/FullTable/TableShowData/TableBody.tsx
@@ -5,8 +5,8 @@ const TableBody = ({table}) => {
     return table.getRowModel().rows.map((row, index) => {
 
         const isEven = index % 2 === 0
-        return <tr key={row.id}
-                   className={`${isEven ? " bg-gray-50 border-b " : " bg-gray-100 border-gray-700"}`}
+        return <ul key={row.id}
+                   className={` ${isEven ? " bg-gray-50  " : " bg-gray-100"} table-row-g table__row__hover`  }
         >
             {row.getVisibleCells().map((cell,index) => {
                 const { columnDef } = cell.column;
@@ -14,23 +14,33 @@ const TableBody = ({table}) => {
                     return null
                 }
                 return (
-                    <td
+                    <li
                         key={index}
+                        // style={{
+                        //     padding: '8px',
+                        //     width: cell.column.getSize(), // Use column size
+                        //     minWidth: columnDef.minWidth, // Use column minSize
+                        //     maxWidth: columnDef.maxWidth, // Use column maxSize
+                        //     // textAlign: 'center',
+                        //     textAlign: columnDef.textAlign,
+                        //     overflow: 'hidden',
+                        // }}
+                        className={"table-cell-g"}
                         style={{
-                            padding: '8px',
-                            width: cell.column.getSize(), // Use column size
-                            minWidth: columnDef.minWidth, // Use column minSize
-                            maxWidth: columnDef.maxWidth, // Use column maxSize
-                            // textAlign: 'center',
-                            textAlign: columnDef.textAlign,
-                            overflow: 'hidden',
+                            padding: '5px 3px',
+                            // textAlign: "center",
+                            width: cell.column.getSize()+"px", // Use column size
+                            overflow:"hidden",
+                            minWidth: cell.column.columnDef.minSize+"px", // Use column minSize
+                            maxWidth: cell.column.columnDef.maxSize+"px", // Use column maxSize
+                            // textOverflow:"ellipsis",
                         }}
                     >
                         {flexRender(columnDef.cell, cell.getContext())}
-                    </td>
+                    </li>
                 );
             })}
-        </tr>
+        </ul>
     })
 
 
