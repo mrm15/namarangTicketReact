9e1508a84affe3ba5ae60c6de6d0b5f1a2197a06
mrm15 <mrm.learn@gmail.com>
2024-09-25 00:12:38 +0330

    change table header

--- src/Components/TableG/FullTable/TableShowData/TableHeader.tsx
+++ src/Components/TableG/FullTable/TableShowData/TableHeader.tsx
@@ -1,42 +1,93 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import {flexRender} from "@tanstack/react-table";
+import {TableGContext} from "../../TableGContext.tsx";
+import {randomNumberGenerator} from "../../../../utils/utilsFunction.tsx";
 
 const TableHeader = ({table}) => {
+    const context = useContext(TableGContext);
+    const { myData, setMyData } = context;
+    const [tableState, setTableState] = useState(0);
+
+
+
     return table.getHeaderGroups()
         .map((headerGroup) => {
             return (
-                <tr
+                <ul
                     key={headerGroup.id}
-                    className={"bg-gray-300"}
+                    className={"bg-gray-300 header__ul table-row-g select-none"}
 
                 >
                     {headerGroup.headers.map((header: any, index: React.Key) => {
 
-                        if(header.column.columnDef.hidden){
+                        if (header.column.columnDef.hidden) {
                             return null
                         }
+
+                        // setInterval(() => {
+                        //     header.column.size += 10;
+                        //     header.column.columnDef.minSize += 10;
+                        //     header.column.columnDef.maxSize += 10;
+                        //
+                        //
+                        //
+                        // }, 2000)
+                        const handleMouseDown = (e, header) => {
+                            const startX = e.clientX;
+                            const startWidth = header.column.getSize();
+
+                            const isRTL=true
+                            const handleMouseMove = (e) => {
+                                // const newWidth = startWidth + (e.clientX - startX);
+                                const deltaX = isRTL ? (startX - e.clientX) : (e.clientX - startX); // Reverse for RTL
+                                const newWidth = startWidth + deltaX;
+                                console.log(newWidth)
+                                header.column.size=newWidth
+                                header.column.columnDef.minSize=newWidth
+                                header.column.columnDef.maxSize=newWidth
+                                // header.column.setSize(newWidth);
+                                setMyData({reload:randomNumberGenerator()})
+                            };
+
+                            const handleMouseUp = () => {
+                                document.removeEventListener('mousemove', handleMouseMove);
+                                document.removeEventListener('mouseup', handleMouseUp);
+                            };
+
+                            document.addEventListener('mousemove', handleMouseMove);
+                            document.addEventListener('mouseup', handleMouseUp);
+                        };
                         return (
-                            <th
+                            <li
                                 key={index}
                                 style={{
                                     padding: '5px 3px',
                                     textAlign: "center",
-                                    width: header.column.getSize()+"", // Use column size
-                                    minWidth: header.column.columnDef.minSize+"", // Use column minSize
-                                    maxWidth: header.column.columnDef.maxSize+"", // Use column maxSize
+                                    overflow: "hidden",
+                                    width: header.column.getSize() + "px", // Use column size
+                                    minWidth: header.column.columnDef.minSize + "px", // Use column minSize
+                                    maxWidth: header.column.columnDef.maxSize + "px", // Use column maxSize
                                 }}
-                                className={"whitespace-nowrap"}
+                                className={"whitespace-nowrap table-cell-g  relative"}
                             >
-                                {flexRender(
-                                    header.column.columnDef.header,
-                                    header.getContext()
-                                )}
-                            </th>
+                                        {flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                    <div
+                                        className={"resizer"}
+
+                                        onMouseDown={(e) => handleMouseDown(e, header)}
+                                    />
+
+                            </li>
                         )
                     })}
-                </tr>
+                </ul>
             )
         })
 };
 
 export default TableHeader
+
+
