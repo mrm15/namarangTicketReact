2bbea6ede7fb83ccf92d4412f789c55dc27d8405
mrm15 <mrm.learn@gmail.com>
2024-09-26 15:55:21 +0330

    add FullAdminSection.tsx for new TableG show tickets

---
+++ src/Components/TableG/FullTable/components/ForwardModalticketTable/FullAdminSection.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import DepartmentList from "./DepartmentList.tsx";
+
+const FullAdminSection = ({mode, departmentList, setSelectedData, userList}) => {
+    return (
+        <div>
+            <div>دسترسی به کل سازمان</div>
+            <div className={' flex gap-2'}>
+                <div className={'w-full'}>
+                    <div>انتخاب دپارتمان</div>
+                    <div>
+                        <DepartmentList
+                            myOptions={departmentList}
+                            myKey={'department'}
+                            setSelectedData={setSelectedData}
+                        />
+                    </div>
+                </div>
+                <div className={'w-full'}>
+                    <div> انتخاب کاربر</div>
+                    {mode === 'admin' && <DepartmentList
+                      myOptions={userList}
+                      myKey={'user'}
+                      setSelectedData={setSelectedData}
+                    />}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FullAdminSection;
