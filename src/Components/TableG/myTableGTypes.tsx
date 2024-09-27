35af56b9fd562dadc9ba4f1fda652032568d9886
mrm15 <mrm.learn@gmail.com>
2024-09-27 14:37:54 +0330

    add types for checked items

--- src/Components/TableG/myTableGTypes.tsx
+++ src/Components/TableG/myTableGTypes.tsx
@@ -1,9 +1,13 @@
-import { ColumnDef } from '@tanstack/react-table';
+import {ColumnDef} from '@tanstack/react-table';
 import React from "react";
 
 // Use intersection to add the `hidden` property to `ColumnDef`
 export type ICustomColumn<TData = any> = ColumnDef<TData> & {
     hidden?: boolean; // Optional hidden property
+    showCheckBoxInHeader?: boolean,
+    type?: "select",
+    uniqId?: string, // برای اینکه چک کنم چک بامس ها بخوره میرم آیدی همون سطر رو با آیدی های موجود توی دیتا چک میکنم ببینم گدوما چک خورده کدوما نخورده
+
 };
 
 export interface IMyData {
@@ -15,11 +19,12 @@ export interface IMyData {
     tableData: any[];
     filters: any[];
     totalRows: number;
-    reload: string| number;
+    reload: string | number;
     isLoading: boolean;
     errorMessage: string;
-    queryData:any
-    reOrderTableAfterChangeColumnWidth:string;
+    queryData: any
+    reOrderTableAfterChangeColumnWidth: string;
+    checkedItems?: any[];
 }
 
 export interface myTableGContext {
