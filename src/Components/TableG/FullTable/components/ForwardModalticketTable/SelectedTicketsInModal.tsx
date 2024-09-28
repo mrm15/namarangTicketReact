29ac702a090f1a3d3d3b0e67cf21e9ff27ede64a
mrm15 <mrm.learn@gmail.com>
2024-09-28 11:32:55 +0330

    edit style to Show ticket Forward Modal

--- src/Components/TableG/FullTable/components/ForwardModalticketTable/SelectedTicketsInModal.tsx
+++ src/Components/TableG/FullTable/components/ForwardModalticketTable/SelectedTicketsInModal.tsx
@@ -4,25 +4,25 @@ const SelectedTicketsInModal = ({selectedItems}) => {
     try{
         return (
 
-            <div className={'ticket__show__in__modal max-h-96 overflow-auto'}>
+            <div className={'ticket__show__in__modal__table'}>
                 {/*{JSON.stringify(selectedItems)}*/}
 
                 <ul>
-                    <li>ردیف</li>
+                    {/*<li>ردیف</li>*/}
                     <li>عنوان</li>
                     <li>شماره تیکت</li>
-                    <li>توضیحات</li>
+                    {/*<li>توضیحات</li>*/}
                 </ul>
                 {selectedItems.map((singleTicket, index) => {
 
 
                     return <ul key={index} className={''}>
-                        <div className={'flex'}>
-                            <li className={'w-1'}>{index + 1}</li>
+
+                            {/*<li className={'w-1'}>{index + 1}</li>*/}
                             <li className={''}>{singleTicket?.title}</li>
                             <li className={''}>{singleTicket?.ticketNumber}</li>
-                            <li className={''}>{singleTicket?.description}</li>
-                        </div>
+                            {/*<li className={''}>{singleTicket?.description}</li>*/}
+
                     </ul>
                 })}
 
