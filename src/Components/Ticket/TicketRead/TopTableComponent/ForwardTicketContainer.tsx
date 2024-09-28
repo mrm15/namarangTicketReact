b38d5919e60ab6a323fd02d1fc2b9f9532726c3f
mrm15 <mrm.learn@gmail.com>
2024-09-28 11:32:17 +0330

    add forward Ticket container

---
+++ src/Components/Ticket/TicketRead/TopTableComponent/ForwardTicketContainer.tsx
@@ -0,0 +1,41 @@
+import React, {useContext, useState} from 'react';
+import ForwardModal from "../../ForwardModal/ForwardModal.tsx";
+import {TableGContext} from "../../../TableG/TableGContext.tsx";
+import ForwardModalTable from "../../../TableG/FullTable/components/ForwardModalticketTable/ForwardModalTable.tsx";
+import {randomNumberGenerator} from "../../../../utils/utilsFunction.tsx";
+
+const ForwardTicketContainer = () => {
+
+    const context = useContext(TableGContext);
+    const {myData, setMyData} = context;
+    const [isOpen, setIsOpen] = useState(false)
+    const closeModal = () => setIsOpen(false)
+    const openModal = () => setIsOpen(true)
+    const setReload = () => {
+        setMyData({reload: randomNumberGenerator()})
+    }
+
+    return (
+        <div>
+
+            <button
+                onClick={openModal}
+                disabled={myData.checkedItems.length === 0}
+                className={"btn-small-show"}
+            >
+                ارجاع چندتایی
+            </button>
+            {isOpen && <ForwardModalTable
+              closeModal={closeModal}
+              currentParams={""}
+              selectedItems={myData.checkedItems}
+              setReload={setReload}
+              title={"اجاع تیکت ها"}
+
+
+            />}
+        </div>
+    );
+};
+
+export default ForwardTicketContainer;
