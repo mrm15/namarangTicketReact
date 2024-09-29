0ed197f5c6fab97005e3b57d9931ac3f8a6c4059
mrm15 <mrm.learn@gmail.com>
2024-09-29 22:13:33 +0330

    add lazy load

--- src/Pages/Pages.tsx
+++ src/Pages/Pages.tsx
@@ -1,36 +1,39 @@
 import {Route, Routes} from "react-router-dom";
-
 import Layout from "../Components/Layout";
 import PersistLogin from "../Components/PersistLogin"
 import RequireAuth from "../Components/RequireAuth"
-import Home from "../Components/Home/Home.tsx"
-import Missing from "../Components/Missing"
 import LoginSMS from "../Components/LoginSMS.tsx";
 import {PAGES} from "./Route-string.tsx"
 import Unauthorized from "../Components/Unauthorized/Unauthorized.tsx";
-import AddUser from "../Components/User/AddUser.tsx";
 import {ROLES} from "./ROLES.tsx";
-import AddRole from "../Components/Role/AddRole.tsx";
-import AddDepartment from "../Components/Department/AddDepartment.tsx";
-import AddStatus from "../Components/Status/AddStatus.tsx";
-import ListStatus from "../Components/Status/ListStatus.tsx";
-import AddFiles from "../Components/Files/AddFiles.tsx";
-import ListFiles from "../Components/Files/ListFiles.tsx";
-import TicketCreate from "../Components/Ticket/TicketCreate/TicketCreate.tsx";
-import {TicketRead} from "../Components/Ticket/TicketRead/TicketRead.tsx";
-import TicketChatList from "../Components/Ticket/TicketChatList/TicketChatList.tsx";
-import AddSettings from "../Components/AdminSettings/AddSettings.tsx";
-import SendSms from "../SendSms/SendSms.tsx";
-import HesabfaTest from "../Components/Test/HesabfaTest.tsx";
-import ShowBill from "../Components/Hesabfa/ShowBill/ShowBill.tsx";
-import SubmitBill from "../Components/Hesabfa/SubmitBill/SubmitBill.tsx";
-import ReportBill from "../ReportBill/ReportBill.tsx";
-import ListUsersG from "../Components/User/ListUsersG.tsx";
-import ListRolesG from "../Components/Role/ListRolesG.tsx";
-import ListDepartmentG from "../Components/Department/ListDepartmentG.tsx";
-import AdminReportCP from "../Components/AdminReport/AdminReportCP.tsx";
-import PackSend from "../Components/PackSend/PackSend.tsx";
-import ScreenShotBill from "../Components/ScreenShotBill/ScreenShotBill.tsx";
+import {lazy, Suspense} from "react"
+import Loader from "../Components/Loader";
+
+const ScreenShotBill = lazy(() => import('../Components/ScreenShotBill/ScreenShotBill.tsx'))
+const PackSend = lazy(() => import("../Components/PackSend/PackSend.tsx"))
+
+const AddUser = lazy(() => import('../Components/User/AddUser.tsx'));
+const Home = lazy(() => import("../Components/Home/Home.tsx"));
+
+const ListUsersG = lazy(() => import('../Components/User/ListUsersG.tsx'));
+const AddRole = lazy(() => import('../Components/Role/AddRole.tsx'));
+const ListRolesG = lazy(() => import('../Components/Role/ListRolesG.tsx'));
+const AddDepartment = lazy(() => import('../Components/Department/AddDepartment.tsx'));
+const ListDepartmentG = lazy(() => import('../Components/Department/ListDepartmentG.tsx'));
+const AddStatus = lazy(() => import('../Components/Status/AddStatus.tsx'));
+const ListStatus = lazy(() => import('../Components/Status/ListStatus.tsx'));
+const AddFiles = lazy(() => import('../Components/Files/AddFiles.tsx'));
+const ListFiles = lazy(() => import('../Components/Files/ListFiles.tsx'));
+const TicketCreate = lazy(() => import('../Components/Ticket/TicketCreate/TicketCreate.tsx'));
+const TicketRead = lazy(() => import('../Components/Ticket/TicketRead/TicketRead.tsx'));
+const TicketChatList = lazy(() => import('../Components/Ticket/TicketChatList/TicketChatList.tsx'));
+const AddSettings = lazy(() => import('../Components/AdminSettings/AddSettings.tsx'));
+const ShowBill = lazy(() => import('../Components/Hesabfa/ShowBill/ShowBill.tsx'));
+const SubmitBill = lazy(() => import('../Components/Hesabfa/SubmitBill/SubmitBill.tsx'));
+const ReportBill = lazy(() => import('../ReportBill/ReportBill.tsx'));
+const AdminReportCP = lazy(() => import('../Components/AdminReport/AdminReportCP.tsx'));
+const HesabfaTest = lazy(() => import('../Components/Test/HesabfaTest.tsx'));
+const Missing = lazy(() => import('../Components/Missing'));
 
 
 const Pages = () => {
@@ -47,10 +50,16 @@ const Pages = () => {
                 {/*<Route path="register" element={<RegisterSMS/>}/>*/}
 
                 <Route path={PAGES.LOGIN} element={<LoginSMS/>}/>
-                <Route path={PAGES.SMS} element={<SendSms/>}/>
-
-                <Route path={`${PAGES.showBill}/`} element={<ShowBill/>}/>
-                <Route path={`${PAGES.showBill}/:factorNumber`} element={<ShowBill/>}/>
+                <Route path={`${PAGES.showBill}/`} element={
+                    <Suspense fallback={<Loader/>}>
+                        <ShowBill/>
+                    </Suspense>
+                }/>
+                <Route path={`${PAGES.showBill}/:factorNumber`} element={
+                    <Suspense fallback={<Loader/>}>
+                        <ShowBill/>
+                    </Suspense>
+                }/>
 
 
                 {/* pages all people can see and need sidebar */}
@@ -59,125 +68,191 @@ const Pages = () => {
                     <Route path="/" element={<Layout/>}>
                         {/* public routes */}
 
-
-                        {/*<Route path="linkpage" element={<LinkPage/>}/>*/}
-                        {/*<Route path="unauthorized" element={<Unauthorized/>}/>*/}
-                        {/*<Route path="SellFactor" element={<SellFactor/>}/>*/}
-                        {/*<Route path="ListProduct" element={<ListProduct/>}/>*/}
-
-                        {/* pages loggedIn users can see and need sidebar */}
-
-                        {/* we want to protect these routes */}
-
-                        <Route path={'/'} element={<Home/>}/>
-                        {/*<Route element={<RequireAuth allowedRoles={ROLES.addContactAccess}/>}>*/}
-                        {/*    <Route path={'/'} element={<Home/>}/>*/}
-                        {/*</Route>*/}
-
+                        <Route path={'/'} element={
+                            <Suspense fallback={<Loader/>}>
+                                <Home/>
+                            </Suspense>
+                        }/>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.user}/>}>
-                            <Route path={PAGES.USER_ADD_EDIT} element={<AddUser/>}/>
-                            <Route path={PAGES.USER_LIST} element={<ListUsersG/>}/>
-
+                            <Route path={PAGES.USER_ADD_EDIT} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddUser/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.USER_LIST} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ListUsersG/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.role}/>}>
-                            <Route path={PAGES.ROLE_ADD_EDIT} element={<AddRole/>}/>
-                            <Route path={PAGES.ROLE_LIST} element={<ListRolesG/>}/>
-
+                            <Route path={PAGES.ROLE_ADD_EDIT} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddRole/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.ROLE_LIST} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ListRolesG/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.department}/>}>
-                            <Route path={PAGES.DEPARTMENT_ADD_EDIT} element={<AddDepartment/>}/>
-                            <Route path={PAGES.DEPARTMENT_LIST} element={<ListDepartmentG/>}/>
+                            <Route path={PAGES.DEPARTMENT_ADD_EDIT} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddDepartment/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.DEPARTMENT_LIST} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ListDepartmentG/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.status}/>}>
-                            <Route path={PAGES.STATUS_ADD_EDIT} element={<AddStatus/>}/>
-                            <Route path={PAGES.STATUS_LIST} element={<ListStatus/>}/>
+                            <Route path={PAGES.STATUS_ADD_EDIT} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddStatus/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.STATUS_LIST} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ListStatus/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.file}/>}>
-                            <Route path={PAGES.FILE_ADD_EDIT} element={<AddFiles/>}/>
-                            <Route path={PAGES.FILE_LIST} element={<ListFiles/>}/>
+                            <Route path={PAGES.FILE_ADD_EDIT} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddFiles/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.FILE_LIST} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ListFiles/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         {/* ticket create */}
                         <Route element={<RequireAuth allowedRoles={ROLES.ticketCreate}/>}>
-                            <Route path={PAGES.ticket_Create} element={<TicketCreate/>}/>
-                            {/* ticketReadOwn تیکت هایی که خودم فرستادم رو ببینم */}
-
-                            <Route path={PAGES.ticket_own_sent} element={<TicketRead view={'readSentTickets'}/>}/>
+                            <Route path={PAGES.ticket_Create} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketCreate/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.ticket_own_sent} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketRead view={'readSentTickets'}/>
+                                </Suspense>
+                            }/>
                         </Route>
-
 
                         {/* ticket readAll for Admin */}
                         <Route element={<RequireAuth allowedRoles={ROLES.ticketReadAll}/>}>
-                            <Route path={PAGES.ticket_Read_All} element={<TicketRead view={'read'}/>}/>
+                            <Route path={PAGES.ticket_Read_All} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketRead view={'read'}/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         {/* -تمام تیکت هایی که من بهشون دسترسی دارم */}
                         <Route element={<RequireAuth allowedRoles={ROLES.ticketReadOwnReceived}/>}>
-                            <Route path={PAGES.ticket_read_my_all_tickets}
-                                   element={<TicketRead view={'readMyAllTickets'}/>}/>
-                        </Route>
-                        <Route element={<RequireAuth allowedRoles={ROLES.readDepartmentTickets}/>}>
-                            <Route path={PAGES.ticket_read_department_tickets}
-                                   element={<TicketRead view={'readDepartmentTickets'}/>}/>
+                            <Route path={PAGES.ticket_read_my_all_tickets} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketRead view={'readMyAllTickets'}/>
+                                </Suspense>
+                            }/>
                         </Route>
 
+                        <Route element={<RequireAuth allowedRoles={ROLES.readDepartmentTickets}/>}>
+                            <Route path={PAGES.ticket_read_department_tickets} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketRead view={'readDepartmentTickets'}/>
+                                </Suspense>
+                            }/>
+                        </Route>
 
                         {/* TicketChatList */}
                         <Route element={<RequireAuth allowedRoles={ROLES.ticketRepliesCreate}/>}>
-                            <Route path={PAGES.ticket_chat_list} element={<TicketChatList/>}/>
+                            <Route path={PAGES.ticket_chat_list} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <TicketChatList/>
+                                </Suspense>
+                            }/>
                         </Route>
 
                         <Route element={<RequireAuth allowedRoles={ROLES.adminSettings}/>}>
-                            <Route path={'hesabfaTest'} element={<HesabfaTest/>}/>
-                            <Route path={PAGES.admin_settings} element={<AddSettings/>}/>
+                            <Route path={'hesabfaTest'} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <HesabfaTest/>
+                                </Suspense>
+                            }/>
+                            <Route path={PAGES.admin_settings} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AddSettings/>
+                                </Suspense>
+                            }/>
                         </Route>
 
-                        {/*<Route element={<RequireAuth allowedRoles={ROLES.smsArchive}/>}>*/}
-                        {/*    <Route path={PAGES.sms_archive} element={<ShowSmsList type={"archive"} />}/>*/}
-                        {/*</Route>*/}
-                        {/*<Route element={<RequireAuth allowedRoles={ROLES.smsPending}/>}>*/}
-                        {/*    <Route path={PAGES.sms_pending} element={<ShowSmsList type={"pending"} />}/>*/}
-                        {/*</Route>*/}
-                        {/*<Route element={<RequireAuth allowedRoles={ROLES.smsSend}/>}>*/}
-                        {/*    <Route path={PAGES.sms_send} element={<SendSmsForm />}/>*/}
-                        {/*</Route>*/}
                         <Route element={<RequireAuth
                             allowedRoles={[...ROLES.submitBillInSubmitOrderForm, ...ROLES.submitBillInChatList]}/>}>
-                            <Route path={PAGES.submit_bill} element={<SubmitBill/>}/>
+                            <Route path={PAGES.submit_bill} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <SubmitBill/>
+                                </Suspense>
+                            }/>
                         </Route>
-
 
                         <Route element={<RequireAuth allowedRoles={ROLES.showReportBillList}/>}>
-                            <Route path={PAGES.reportBill} element={<ReportBill/>}/>
+                            <Route path={PAGES.reportBill} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ReportBill/>
+                                </Suspense>
+                            }/>
                         </Route>
+
                         <Route element={<RequireAuth allowedRoles={ROLES.adminReport}/>}>
-                            <Route path={PAGES.adminReport} element={<AdminReportCP/>}/>
+                            <Route path={PAGES.adminReport} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <AdminReportCP/>
+                                </Suspense>
+                            }/>
                         </Route>
-                        <Route element={<RequireAuth allowedRoles={[...ROLES.basted_bandi_ersal , ...ROLES.screenShotBills]}/>}>
-                            <Route path={PAGES.basted_bandi_ersal} element={<PackSend/>}/>
+
+                        <Route element={<RequireAuth
+                            allowedRoles={[...ROLES.basted_bandi_ersal, ...ROLES.screenShotBills]}/>}>
+                            <Route path={PAGES.basted_bandi_ersal} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <PackSend/>
+                                </Suspense>
+                            }/>
                         </Route>
+
                         <Route element={<RequireAuth allowedRoles={ROLES.screenShotBills}/>}>
-                            <Route path={PAGES.screenshot} element={<ScreenShotBill/>}/>
+                            <Route path={PAGES.screenshot} element={
+                                <Suspense fallback={<Loader/>}>
+                                    <ScreenShotBill/>
+                                </Suspense>
+                            }/>
                         </Route>
+
                         <Route path={'/unauthorized'} element={<Unauthorized/>}/>
                     </Route>
 
                     {/* catch all */}
                 </Route>
-                <Route path="*" element={<Missing/>}/>
-
-                {/*<Route path="/" element={<Dashboard/>}>*/}
-                {/*    <Route path="/" element={<MainDashboardPage/>}/>*/}
-                {/* Namarang */}
-                {/*<Route path="SellFactor" element={<SellFactor/>}/>*/}
-                {/*<Route path="AddProduct" element={<AddProduct/>}/>*/}
-                {/*<Route path="ListProduct" element={<ListProduct/>}/>*/}
-                {/*</Route>*/}
+                <Route path="*" element={
+                    <Suspense fallback={<Loader/>}>
+                        <Missing/>
+                    </Suspense>
+                }/>
             </Routes>
         </>
     );
