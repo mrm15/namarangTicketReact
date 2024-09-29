d2d60748e7bcf27f68068cbba13d1a21e2764275
mrm15 <mrm.learn@gmail.com>
2024-09-29 17:13:05 +0330

    update

--- src/Components/ScreenShotBill/ScreenShotBill.tsx
+++ src/Components/ScreenShotBill/ScreenShotBill.tsx
@@ -1,19 +1,49 @@
-import React, {useEffect} from 'react';
-import {useLocation} from "react-router-dom";
+import React, {useEffect, useMemo} from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
+import {ScreenshotProvider, useScreenshotContext} from "./ScreenShotContext.tsx";
+import PictureSection from "./PictureSection.tsx";
+import ScreenShotButton from "./ScreenShotButton.tsx";
+import ScreenShotTable from "./ScreenShotTable.tsx";
+import {findNewItemArray} from "./function.tsx";
+import CalculateData from "./CalculateData.tsx";
+import {PAGES} from "../../Pages/Route-string.tsx";
 
 const ScreenShotBill = () => {
+
+    const initialData = {
+        billData: "",
+        dateOnScreenShot: "",
+        description: "",
+        fileNumber: "",
+        fileName: "",
+        fileNumberText: "",
+        fileNameUnique: "",
+        tableColumnArray: [],
+
+    }
+
     const myLocation = useLocation();
+    const navigateTo = useNavigate()
+    if (!myLocation.state.data) {
+        navigateTo(PAGES.DASHBOARD)
+    }
 
-
-    useEffect(() => {
-        const singleBill = myLocation?.state?.data?.bill
-
-    }, [myLocation])
 
     return (
-        <div>
-            من قراره صفحه ی اسکرین شات باشم
-        </div>
+        <ScreenshotProvider initialData={initialData}>
+            <CalculateData/>
+            <div id={'i_want_to_take_screenshot_here'}
+                 style={{
+                     background: "white",
+                     border: ` 5px solid rgba(255, 0, 0, 0.13)`,
+                     width: "100%",
+                 }}
+            >
+                <PictureSection/>
+                <ScreenShotTable/>
+            </div>
+            <ScreenShotButton/>
+        </ScreenshotProvider>
     );
 };
 
