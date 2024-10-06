2e9d48c8a60f4fcdb1a966c18e38c33c7e936ffc
mrm15 <mrm.learn@gmail.com>
2024-10-07 01:18:57 +0330

    Update AdminReport.tsx

--- src/Components/AdminReport/AdminReport.tsx
+++ src/Components/AdminReport/AdminReport.tsx
@@ -2,7 +2,6 @@ import React, {useContext, useEffect, useState} from 'react';
 import {AdminReportContext} from './AdminReportContext.tsx';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate.tsx';
 import SummaryReport from './SummaryReport.tsx';
-import ReportTable from './ReportTable/ReportTable.tsx';
 import AdminReportFilter from "./AdminReportFilter.tsx";
 import {formatDateForBackend} from "../../utils/utilsFunction.tsx";
 import {useQuery} from "@tanstack/react-query";
