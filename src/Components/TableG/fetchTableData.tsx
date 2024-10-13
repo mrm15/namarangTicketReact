b60fdd7d8c4d364f3673a5bae207219e0eb6c168
mohammad mirarab <mrm.learn@gmail.com>
2024-10-13 17:17:22 +0330

    update

--- src/Components/TableG/fetchTableData.tsx
+++ src/Components/TableG/fetchTableData.tsx
@@ -1,4 +1,11 @@
 export const fetchTableData = async (url: string, myAxios: any, page: number, pageSize: number, filters: any) => {
+    console.log("==============")
+    console.log("url: " + url)
+    console.log("myAxios: " + myAxios)
+    console.log("page: " + page)
+    console.log("pageSize: " + pageSize)
+    console.log("filters: " + filters)
+    console.log("==============")
     const data = {
         page: page,
         pageSize: pageSize,
