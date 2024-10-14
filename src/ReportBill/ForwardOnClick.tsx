90aa25bf3746523fb71f6d4a2c8e087e3503fb25
mohammad mirarab <mrm.learn@gmail.com>
2024-10-14 12:40:56 +0330

    Update ForwardOnClick.tsx

--- src/ReportBill/ForwardOnClick.tsx
+++ src/ReportBill/ForwardOnClick.tsx
@@ -2,11 +2,14 @@ import {useNavigate} from "react-router-dom";
 import {PAGES} from "../Pages/Route-string";
 import React from "react";
 
-const ForwardOnClick = ({value, NewPage, options , buttonCaption="ویرایش"}) => {
+const ForwardOnClick = ({value, NewPage, options , buttonCaption="ویرایش",
+                            className="",
+}) => {
 
     const navigateTo = useNavigate();
     try {
         return <button
+            className={className}
             onClick={() => navigateTo(NewPage, options)}>
             {/*{value}*/}
             {buttonCaption}
