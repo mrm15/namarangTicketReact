978a169379e292ea4e112856bb2c344af554a027
mrm15 <mrm.learn@gmail.com>
2024-10-18 18:42:59 +0330

    update logic

--- src/Components/TableG/FullTable/Filters/DateFilter/removeItemFromFilterArray.tsx
+++ src/Components/TableG/FullTable/Filters/DateFilter/removeItemFromFilterArray.tsx
@@ -1,13 +1,13 @@
 const removeItemFromFilterArray = ({property, myData, setMyData}) => {
     // چ.م اینجا خودم در نظر گرفتم که یه عدد 1 -2 اضافه میکنم به مقدار Date پیس شامل Date باشه باید پاک بشه
 
-    setTimeout(() => {
+
         const newFilterArray = myData?.filters?.filter(row => row.property.includes(property))
 
         setMyData({filters: newFilterArray})
 
-        setTimeout(() => console.log("ttt" ,  myData.filters), 5000)
-    }, 100)
+        // setTimeout(() => console.log("ttt" ,  myData.filters), 5000)
+
 
 
 }
