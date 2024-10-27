767ac04fe5bcac5fac1c65642ee4c69b83c8abb5
mrm15 <mrm.learn@gmail.com>
2024-10-28 00:17:17 +0330

    add a little spinner

---
+++ src/Components/Loader/LittleSpinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LittleSpinner = () => {
+    return (
+        <div className="flex items-center justify-center">
+            <div className="animate-spin">&#9696;</div>
+        </div>
+    )
+};
+
+export default LittleSpinner;
