97a87bf1ed8849b67f165672907df594959a7b3b
mrm15 <mrm.learn@gmail.com>
2024-10-27 23:46:17 +0330

    update ticket read

--- src/Components/Ticket/TicketRead/TicketRead.tsx
+++ src/Components/Ticket/TicketRead/TicketRead.tsx
@@ -1,42 +1,9 @@
-import {PAGES} from "../../../Pages/Route-string";
-import {useEffect, useState} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
 import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
-import {toast} from "react-toastify";
-import DeleteButton from "../../../assets/icons/DeleteButton";
-import Loader from "../../Loader";
-import AggridDataShow from "../../AgGridDataShow/AgGridDataShow";
 import useAuth from "../../../hooks/useAuth.tsx";
-import {FaShareSquare} from "react-icons/fa";
-import ForwardModal from "../ForwardModal/ForwardModal.tsx";
 import {useQuery} from "@tanstack/react-query";
 import TableG from "../../TableG/TableG.tsx";
 import TopTableComponent from "./TopTableComponent/TopTableComponent.tsx";
 
-
-interface ColumnDefinition {
-    minWidth: number;
-    headerName: string;
-    field: string;
-    cellStyle?: (params: any) => any; // Define cellStyle as an optional function
-}
-
-// interface TicketReadProps {
-//     view?: string;
-//     [key: any]: any;
-//
-// }
-
-type MyStateType = {
-    [key: string]: any;
-};
-
-const title = {
-    readSentTickets: 'لیست پیام های ارسالی',
-    read: 'همه ی تیکت های موجود در سیستم',
-    readMyAllTickets: 'همه ی تیکت های من',
-    readDepartmentTickets: 'تیکت های دپارتمان',
-}
 
 function TicketRead({view}) {
     const {auth} = useAuth();
