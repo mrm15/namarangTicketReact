1b4dde9903089609f500b1baae6c7b47edef8f1e
mrm15 <mrm.learn@gmail.com>
2024-10-28 00:58:21 +0330

    add modal

--- src/Components/Ticket/TicketRead/TopTableComponent/MarkAsReadTicketAssignments.tsx
+++ src/Components/Ticket/TicketRead/TopTableComponent/MarkAsReadTicketAssignments.tsx
@@ -1,18 +1,57 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {TableGContext} from "../../../TableG/TableGContext.tsx";
+import Modal from "../../../Modal/Modal.tsx";
 
 const MarkAsReadTicketAssignments = () => {
     const context = useContext(TableGContext);
     const {myData, setMyData} = context;
     // myData.tableData
     // myData.checkedItems
-    console.log(myData.columns)
-    debugger
+    // console.log(myData.columns)
+    console.log(myData.checkedItems)
+
+
+
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const closeModal = () => setIsOpenModal(false)
+    const openModal = () => setIsOpenModal(true)
+
 
     return (
-        <button className={"btn-white-border-mir"}>
-            خوانده شده
-        </button>
+        <>
+            {isOpenModal && (
+                <Modal
+                    showButtons={false}
+                    closeModal={closeModal}
+                    title={"خواندن همه ی پیام ها"}
+                >
+                    <div>
+                        آیا مطمئنی که میخوای همه ی پیام ها رو به وضعیت
+                        &nbsp;
+                        <b> «خوانده شده»</b>
+                        &nbsp;
+                        تغییر بدی؟
+                    </div>
+                    <div className={"flex gap-2 w-full  justify-center mt-10"}>
+                        <button className={"btn-green-mir"}>
+                            آره
+                            👍🏿
+                        </button>
+                        <button className={"btn-red-border-mir"}>
+نه نه نه                            🙅🏿‍♀️
+                        </button>
+                    </div>
+
+                </Modal>
+            )}
+            <button className={"btn-white-border-mir"}
+
+                    onClick={openModal}
+
+            >
+                خواندن همه
+            </button>
+        </>
     );
 };
 
