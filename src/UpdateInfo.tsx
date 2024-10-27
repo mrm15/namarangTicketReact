757a25fd5d06f033290b8659e661a082a8036181
mrm15 <mrm.learn@gmail.com>
2024-10-28 01:07:09 +0330

    do not show message on send message from service worker

--- src/UpdateInfo.tsx
+++ src/UpdateInfo.tsx
@@ -5,18 +5,18 @@ const UpdateInfo = () => {
     const [isModalOpen, setModalOpen] = useState(false);
 
     useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.addEventListener('message', (event) => {
-                if (event.data && event.data.type === 'CACHE_UPDATED') {
-                    // Show the modal when a new version is available
-                    //setModalOpen(true);
-                    alert(`
-                    قابلیت نمایش ستون جمع فاکتور برای تایید کردن فاکتور توی این آپدیت اضافه شد.
-                    
-                    `)
-                }
-            });
-        }
+        // if ('serviceWorker' in navigator) {
+        //     navigator.serviceWorker.addEventListener('message', (event) => {
+        //         if (event.data && event.data.type === 'CACHE_UPDATED') {
+        //             // Show the modal when a new version is available
+        //             //setModalOpen(true);
+        //             alert(`
+        //             قابلیت نمایش ستون جمع فاکتور برای تایید کردن فاکتور توی این آپدیت اضافه شد.
+        //
+        //             `)
+        //         }
+        //     });
+        // }
     }, []);
 
     return <div></div>
