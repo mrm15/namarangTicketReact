0db0b8628d529c0c247e8b534628a8792a6fc978
mohammad mirarab <mrm.learn@gmail.com>
2024-10-29 17:07:51 +0330

    update

---
+++ src/Components/Hesabfa/SubmitBill2/initialDataTypes.tsx
@@ -0,0 +1,67 @@
+// Define the Unit interface for Units array
+export interface IUnit {
+    id: number;
+    value: string;
+    divideNumber: number;
+}
+
+export interface IOther {
+    Title: string;
+    Amount?: string;
+    Add?: boolean;
+}
+
+// Define the InvoiceItem interface
+export interface IInvoiceItem {
+    RowId: number;
+    Id?: number;
+    Description?: string;
+    ItemCode?: string;
+    Unit?: string;
+    Quantity?: number;
+    UnitPrice?: number;
+    Discount?: number;
+    Tax?: number;
+    SubUnit?: string;
+    Name?: string;
+    fixedPrice?: number;
+    dividedBy?: number;
+    // selectedUnit?: string;
+    Units?: IUnit[];
+    sum?: number;
+}
+
+// Define the Invoice interface
+export interface IInvoice {
+    Contact: any;
+    Number: string;
+    ContactTitle: string;
+    Reference: string;
+    Date: string;
+    DueDate: string;
+    ContactCode: string;
+    Note: string;
+    InvoiceType: number;
+    Status: number;
+    Tag: string;
+    InvoiceItems?: IInvoiceItem[] | [];
+    Others: IOther[];
+    Currency: string;
+    TaxId: string;
+    CurrencyRate: number;
+    Project: "";
+    Sum: number;
+}
+
+export interface IProjectList {
+    Id: number;
+    Title: string;
+    Active: boolean;
+}
+
+export interface IInitialBillData {
+    productList?: any[],
+    projectList?: IProjectList[] | [],
+    customerList?: any[],
+
+}
