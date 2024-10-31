6ae35d725fdb688900a9922a17de035ca61fbda0
mohammad mirarab <mrm.learn@gmail.com>
2024-10-31 21:14:31 +0330

    update submit bill 2

--- src/Components/Hesabfa/SubmitBill2/components/EmptyRow.tsx
+++ src/Components/Hesabfa/SubmitBill2/components/EmptyRow.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import ShowProductListForSelect from "./ShowProductListForSelect.tsx";
 
 const EmptyRow = () => {
     return (
         <>
+            {/*<tr>*/}
+            {/*    {[0, 0, 0, 0, 0, 0, 0, 0].map((r, index) => <td key={index}></td>)}*/}
+            {/*</tr>*/}
             <tr>
-                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((r,index) => <td key={index}><div>&nbsp;</div></td>)}
+                <td>
+                    <div></div>
+                </td>
+                <td>
+                    <section style={{
+                        // minWidth:"450px",
+                        maxWidth:"450px",
+                        // width:"450px",
+                        // background:"red",
+                    }}>
+                        <ShowProductListForSelect/>
+                    </section>
+                </td>
+                {[0, 0, 0, 0, 0, 0].map((r, index) => <td key={index}></td>)}
             </tr>
-            <tr>
-                {[0, 0, 0, 0, 0, 0, 0, 0, 0].map((r,index) => <td key={index}><div>&nbsp;</div></td>)}
-            </tr>
+            {/*<tr>*/}
+            {/*    {[0, 0, 0, 0, 0, 0, 0, 0].map((r, index) => <td key={index}></td>)}*/}
+            {/*</tr>*/}
 
         </>
     );
