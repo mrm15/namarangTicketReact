2caf83de3e2c0e3f628fd31bb5a4dc3ef92191f7
mohammad mirarab <mrm.learn@gmail.com>
2024-11-02 15:31:06 +0330

    update

--- src/Components/AccountingReports/AccountingReports.tsx
+++ src/Components/AccountingReports/AccountingReports.tsx
@@ -4,16 +4,25 @@ import {useQuery} from "@tanstack/react-query";
 import ReportFilters from "./components/ReportFilters.tsx";
 import {AccountingReportsProvider, useAccountingReportsContext} from "./AccountingReportsContext.tsx";
 import AccountingComponent from "./components/AccountingComponent.tsx";
+import {dateObjectToIso8601} from "../../utils/utilsFunction.tsx";
+import  {DateObject} from "react-multi-date-picker";
+
 
 const AccountingReports = () => {
 
 
+    const currentDate = new Date();
+    const hesabfaFormatDate = dateObjectToIso8601(new DateObject())
+
+    const tomorrowJsDate = new Date(currentDate);
+    tomorrowJsDate.setDate(currentDate.getDate() + 1);
+    const hesabfaFormatTomorrow = dateObjectToIso8601(new DateObject({ date: tomorrowJsDate }));
 
     const initialData11 = {
         data: [],
         filters: [
-            {uniqId: "startDate", Property: "Date", Operator: "<=", Value: "", showValue: new Date()},
-            {uniqId: "endDate", Property: "Date", Operator: ">", Value: "", showValue: new Date()},
+            {uniqId: "startDate", Property: "Date", Operator: "<=", Value: hesabfaFormatDate, showValue: currentDate},
+            {uniqId: "endDate", Property: "Date", Operator: ">", Value: hesabfaFormatTomorrow, showValue: tomorrowJsDate},
         ]
     }
 
