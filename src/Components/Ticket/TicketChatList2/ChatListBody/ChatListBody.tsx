d7d4bfbec42023cb73619282ea801b104bab0d64
mrm15 <mrm.learn@gmail.com>
2024-11-06 03:41:52 +0330

    update

--- src/Components/Ticket/TicketChatList2/ChatListBody/ChatListBody.tsx
+++ src/Components/Ticket/TicketChatList2/ChatListBody/ChatListBody.tsx
@@ -3,6 +3,10 @@ import {useChatListContext} from '../ChatListContext';
 import {FaFile} from 'react-icons/fa';
 import ManPerson from "../../../../assets/Svg/ManPerson.tsx";
 import "./telegram-bg-style.scss"
+import {MdOutlinePersonSearch} from "react-icons/md";
+import {PiDetectiveFill} from "react-icons/pi";
+import {RiSearchFill} from "react-icons/ri";
+import BillDataButtonInChatList from "./BillDataButtonInChatList.tsx";
 
 const ChatListBody: React.FC = () => {
     const {data} = useChatListContext();
@@ -11,9 +15,21 @@ const ChatListBody: React.FC = () => {
         <div className={"telegram__bg__style"}>
             <div className={"telegram__color__bg"}>
                 <div className=" flex-1 overflow-y-auto p-4 ltr">
-                    {data?.data?.map((item, index) => {
+                    {data?.data?.map((item:any, index) => {
                         const isSent = item.isTicketSender;
                         const isHidden = item.visibleToUser === false;
+
+                        const isTicketSender = item.isTicketSender;
+                        const isVisibleToUser = item.visibleToUser
+                        const billNumber = item?.billNumber
+                        const billStatus = item?.billStatus
+                        const type = item?.type // تایپ رو گرفتم تا بدونم  این تیکت هست یا توی تیکت ریپلای که حذف کردنش آسون تر باشه
+                        const id = item.id // آیدی رو گرفتم که بتونم موقع حذف بدونم چیو حذف کنم
+                        const tempBillData = {
+                            billNumber, billStatus, type, id,
+                            ticketId: data?.ticketId, // این از توی دیتا میاد
+                            ticketNumber: data.ticketNumber, // اینم از توی دیتا میاد
+                        }
 
                         return (
                             <div key={index} className={` flex ${isSent ? 'justify-end' : 'justify-start'} mb-4`}
@@ -21,7 +37,7 @@ const ChatListBody: React.FC = () => {
                             >
                                 <div className={`${isSent ? 'text-right' : 'text-left'} w-10/12 lg:w-1/2`}>
                                     {/* User info and hidden message indicator */}
-                                    {isHidden && <div className="text-xs text-gray-400 mb-1">پیام مخفی</div>}
+
                                     <div className="flex items-center mb-1">
                                         {!isSent && (
                                             <div className="w-8 h-8 mr-2">
@@ -31,14 +47,23 @@ const ChatListBody: React.FC = () => {
                                         <div
                                             className="text-xs text-gray-500 bg-gray-200 px-2 py-1 rounded">{item.user_name} • {item.department_name}</div>
                                     </div>
-
                                     {/* Message Bubble */}
+
+                                    <div className={"transform transition-transform duration-300 hover:translate-x-4"}>
+                                        {isHidden && <div className=" flex justify-end">
+                                          <RiSearchFill size={24} color="black"/>
+                                          <PiDetectiveFill size={24} color="black"/>
+                                        </div>}
+                                    </div>
                                     <div
                                         className={`p-3 rounded-lg shadow-md ${
                                             isSent ? 'rounded-tr-none text-black' : 'bg-gray-200 text-gray-800 rounded-tl-none'
-                                        } ${isHidden ? 'opacity-30' : ''}`}
+                                        }
+                                         ${isHidden ? 'border border-black ' : ''}`}
                                         style={isSent ? {backgroundColor: 'hsl(114, 93.75%, 81.73%)'} : {}}
                                     >
+
+
                                         <p>{item.description}</p>
 
                                         {/* File Attachments */}
@@ -57,6 +82,7 @@ const ChatListBody: React.FC = () => {
                                                         className={`flex items-center p-2 border rounded-lg ${
                                                             isPhoto ? 'flex-col' : 'flex-row'
                                                         } ${isPhoto ? '' : 'space-x-2'} bg-white shadow`}
+                                                        download={true}
                                                     >
                                                         {isPhoto ? (
                                                             <img src={href} alt={file.fileName}
@@ -70,6 +96,13 @@ const ChatListBody: React.FC = () => {
                                                     </a>
                                                 );
                                             })}
+                                        </div>
+                                        <div>
+                                            {billNumber && <BillDataButtonInChatList billData={tempBillData}
+                                                                                     setReload={undefined}
+
+                                            />}
+
                                         </div>
                                         {/* Timestamp */}
                                         <div className="text-xs text-gray-400 mt-1 w-fit">
