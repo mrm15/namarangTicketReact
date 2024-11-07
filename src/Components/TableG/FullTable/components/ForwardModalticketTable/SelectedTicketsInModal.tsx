321b4f2e1dec8a22424f5b8cff90e1b5b1333c78
mrm15 <mrm.learn@gmail.com>
2024-11-08 02:41:30 +0330

    change style

--- src/Components/TableG/FullTable/components/ForwardModalticketTable/SelectedTicketsInModal.tsx
+++ src/Components/TableG/FullTable/components/ForwardModalticketTable/SelectedTicketsInModal.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
 
-const SelectedTicketsInModal = ({selectedItems}) => {
-    try{
+const SelectedTicketsInModal = ({ selectedItems }) => {
+    try {
         return (
-
-            <div className={'ticket__show__in__modal__table'}>
-                {/*{JSON.stringify(selectedItems)}*/}
-
-                <ul>
-                    {/*<li>ردیف</li>*/}
+            <div className="ticket__show__in__modal__table  max-w-md mx-auto">
+                <ul className="flex justify-between items-center border-b-2 pb-2 mb-2 text-gray-700 text-sm">
                     <li>عنوان</li>
                     <li>شماره تیکت</li>
-                    {/*<li>توضیحات</li>*/}
                 </ul>
-                {selectedItems.map((singleTicket, index) => {
-
-
-                    return <ul key={index} className={''}>
-
-                            {/*<li className={'w-1'}>{index + 1}</li>*/}
-                            <li className={''}>{singleTicket?.title}</li>
-                            <li className={''}>{singleTicket?.ticketNumber}</li>
-                            {/*<li className={''}>{singleTicket?.description}</li>*/}
-
+                {selectedItems.map((singleTicket, index) => (
+                    <ul
+                        key={index}
+                        className="flex justify-between items-center border-b py-2 text-gray-600 hover:bg-blue-50 transition-all"
+                    >
+                        <li className="text-sm font-medium">{singleTicket?.title}</li>
+                        <li className="text-sm">{singleTicket?.ticketNumber}</li>
                     </ul>
-                })}
-
-
+                ))}
             </div>
         );
-    }catch (error){
-        return <div>{error.toString()}</div>
+    } catch (error) {
+        return <div className="text-red-500">{error.toString()}</div>;
     }
 };
 
