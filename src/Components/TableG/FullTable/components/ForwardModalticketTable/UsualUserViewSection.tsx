43daeb86076414ba9a8db3f8c0328457f8a1348e
mrm15 <mrm.learn@gmail.com>
2024-11-08 02:41:18 +0330

    change style

--- src/Components/TableG/FullTable/components/ForwardModalticketTable/UsualUserViewSection.tsx
+++ src/Components/TableG/FullTable/components/ForwardModalticketTable/UsualUserViewSection.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import SelectOptionListView from "./SelectOptionListView.tsx";
 
-const UsualUserViewSection = ({mode, setSelectedData, destinationUserList}) => {
+const UsualUserViewSection = ({ mode, setSelectedData, destinationUserList }) => {
     return (
-        <div>
-            <div>
-                <div className={' flex gap-2'}>
-                    <div className={'w-full'}>
-                        <div> انتخاب کاربر</div>
-                        <SelectOptionListView
-                            myOptions={destinationUserList}
-                            myKey={'user'}
-                            setSelectedData={setSelectedData}
-                        />
-                    </div>
-                </div>
+        <div className="max-w-lg mx-auto p-4 bg-white shadow-md rounded-lg">
+            <div className="text-lg font-semibold text-gray-700 mb-2">انتخاب کاربر</div>
+            <div className="p-3 bg-gray-50 rounded-md border border-gray-200">
+                <SelectOptionListView
+                    myOptions={destinationUserList}
+                    myKey="user"
+                    setSelectedData={setSelectedData}
+                />
             </div>
-
         </div>
     );
 };
