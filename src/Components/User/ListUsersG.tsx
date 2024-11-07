abd60addf6d3ddef6b7f5f946d69104ecb8de213
mohammad mirarab <mrm.learn@gmail.com>
2024-11-07 10:33:48 +0330

    move button

--- src/Components/User/ListUsersG.tsx
+++ src/Components/User/ListUsersG.tsx
@@ -9,6 +9,8 @@ import {PAGES} from "../../Pages/Route-string.tsx";
 import {toast} from "react-toastify";
 import {HeaderItem} from "../../utils/types/types.ts";
 import TableG from "../TableG/TableG";
+import useAuth from "../../hooks/useAuth.tsx";
+import {IoPersonAddSharp} from "react-icons/io5";
 
 interface ColumnDef {
     headerName: string;
@@ -29,13 +31,33 @@ function ListUsers() {
 
     const [isLoading, setIsLoading] = useState(true)
     const [reload, setReload] = useState(1)
-
+    // {
+    //     name: "افزودن کاربر",
+    //     link: PAGES.USER_ADD_EDIT,
+    //     icon: IoPersonAddSharp,
+    //     showItem: roleAccessList?.includes('userCreate'),
+    // },
+    const navigateTo = useNavigate()
+    const {auth} = useAuth();
+    const roleAccessList = auth.userInfo?.roleAccessList;
+    const showAddButton = roleAccessList?.includes('userCreate')
 
     try {
         return (
-            <TableG
-                url={"/user/read"}
-            />
+            <div>
+                {showAddButton && <button
+                    onClick={() => navigateTo(PAGES.USER_ADD_EDIT)}
+                    className={"btn-white-border-mir my-2"}
+                >
+                   <div className={"flex gap-2 items-center"}>
+                       <IoPersonAddSharp size={20} />
+                       <span> افزودن کاربر</span>
+                   </div>
+                </button>}
+                <TableG
+                    url={"/user/read"}
+                />
+            </div>
         );
     } catch (error) {
         return <>{error.toString()}</>
