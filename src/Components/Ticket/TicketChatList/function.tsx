f96016dd7f6e5d95edd5551b91f50b9fa4c78cd7
mrm15 <mrm.learn@gmail.com>
2024-11-18 23:04:24 +0330

    update

--- src/Components/Ticket/TicketChatList/function.tsx
+++
@@ -1,5 +0,0 @@
-export const isAPhoto = (fileName: string) => {
-    const photoExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp', '.tiff', '.svg'];
-    const extension = fileName.slice(fileName.lastIndexOf('.')).toLowerCase();
-    return photoExtensions.includes(extension);
-}
