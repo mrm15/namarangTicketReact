ce415984606dba36cb73d87182521a92c78f90c5
mohammad mirarab <mrm.learn@gmail.com>
2024-11-26 11:52:55 +0330

    change style

--- src/Components/Test/HesabfaTest.tsx
+++ src/Components/Test/HesabfaTest.tsx
@@ -85,119 +85,109 @@ const HesabfaTest = () => {
 
     const sendNotificationTest = async () => {
         //
-        const result = await myAxiosPrivate.post('subscribe/test', {data:"15"})
+        const result = await myAxiosPrivate.post('subscribe/test', {data: "15"})
         showToast(result)
     }
 
 
-    return (
+    try {
 
-        <div className={"bg-black  rounded h-screen overflow-scroll"}>
 
-            <div className={"bg-black text-red-600 text-center fontSize22 ltr "}>
-                دسترسی فقط مدیر کل سایت
+        return (
+            <div className="bg-gray-800 rounded h-screen overflow-scroll">
+                <div className=" text-red-600 text-center text-lg sm:text-xl font-bold py-4">
+                    دسترسی فقط مدیر کل سایت
+                </div>
+                <div className="p-4 sm:p-8 font-light flex flex-wrap gap-4 justify-center">
+                    <UpdateLocalBills/>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={handleGetProduct}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            تست دریافت کالا
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={handleGetProjects}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            تست دریافت پروژه ها
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={() => handleGetBillData("1003")}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            تست دریافت اطلاعات یک فاکتور
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={handleGetCustomerList}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            تست دریافت لیست مخاطبین
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={testSubmitFactor}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            ثبت فاکتور تستی
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={testGetContactData}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            گرفتن دیتای یک مخاطب
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={syncContactsFromHesabfa}
+                            className="btn-submit-mir w-full sm:w-auto"
+                        >
+                            همگام سازی مخاطبین
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={updateProductsFromHesabfaToSite}
+                            className="btn-submit-mir bg-red-900 w-full sm:w-auto"
+                        >
+                            به روز رسانی کالاهای حسابفا و سایت
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={sendSmsCurrentReport}
+                            className="btn-submit-mir bg-red-900 w-full sm:w-auto"
+                        >
+                            ارسال گزارش مدیریتی
+                        </button>
+                    </div>
+                    <div className="w-full sm:w-auto">
+                        <button
+                            onClick={sendNotificationTest}
+                            className="btn-submit-mir bg-red-900 w-full sm:w-auto"
+                        >
+                            تست نوتیفیکشن سایت برای شماره خودم!!
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className={'p-32 font-light flex flex-wrap gap-2 animate-pulse'}>
-                <UpdateLocalBills/>
-                <div>
-                    <button
-                        onClick={handleGetProduct}
-                        className={'btn-submit-mir'}>
-                        تست دریافت کالا
-                    </button>
-                </div>
-                <div>
-                    <button
-                        onClick={handleGetProjects}
-                        className={'btn-submit-mir'}
-
-                    >
-                        تست دریافت پروژه ها
-                    </button>
-                </div>
-                <div>
-                    <button
-                        onClick={() => handleGetBillData("1003")}
-                        className={'btn-submit-mir'}
-
-                    >
-                        تست دریافت اطلاعات یک فاکتور
-                    </button>
-                </div>
-                <div>
-                    <button
-                        onClick={handleGetCustomerList}
-                        className={'btn-submit-mir'}
-
-                    >
-                        تست دریافت لیست مخاطبین
-                    </button>
-                </div>
-                <div>
-                    <button
-                        onClick={testSubmitFactor}
-                        className={'btn-submit-mir'}
-
-                    >
-                        ثبت فاکتور تستی
-                    </button>
-                </div>
-
-                <div>
-                    <button
-                        onClick={testGetContactData}
-                        className={'btn-submit-mir'}
-
-                    >
-                        گرفتن دیتای یک مخاطب
-                    </button>
-                </div>
-
-                <div>
-                    <button
-                        onClick={syncContactsFromHesabfa}
-                        className={'btn-submit-mir'}
-
-                    >
-                        همگام سازی مخاطبین
-                    </button>
-                </div>
-
-                <div>
-                    <button
-                        onClick={updateProductsFromHesabfaToSite}
-                        className={'btn-submit-mir bg-red-900'}
-
-                    >
-                        به روز رسانی کالاهای حسابفا و سایت
-                    </button>
-                </div>
+        )
 
 
-                <div>
-                    <button
-                        onClick={sendSmsCurrentReport}
-                        className={'btn-submit-mir bg-red-900'}
-                    >
-                        ارسال گزارش مدیریتی
-                    </button>
-                </div>
-
-                <br/>
-                <br/>
-                <div>
-                    <button
-                        onClick={sendNotificationTest}
-                        className={'btn-submit-mir bg-red-900'}
-                    >
-                        تست نوتیفیکشن سایت برای شماره خودم!!
-                    </button>
-                </div>
-
-
-            </div>
-        </div>
-    );
+    } catch (error) {
+        return <>{error.toString()}</>
+    }
 };
 
 export default HesabfaTest;
