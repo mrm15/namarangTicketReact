016dbac81bc6b0056f4eae53f26ecba0fd86839d
mohammad mirarab <mrm.learn@gmail.com>
2024-12-01 10:36:21 +0330

    fix first initialize data Date

--- src/Components/Bank/Bank.tsx
+++ src/Components/Bank/Bank.tsx
@@ -1,15 +1,23 @@
 import {BankProvider} from "./bankContext";
 import BankData from "./BankData/BankData";
+import {dateObjectToIso8601} from "../../utils/utilsFunction.tsx";
+import {DateObject} from "react-multi-date-picker";
+
 
 const Bank = () => {
 
-    const currentDate = new Date();
-    const tomorrowJsDate = new Date(currentDate);
-    tomorrowJsDate.setDate(currentDate.getDate() + 1);
+
+    // Create a DateObject instance representing today at midnight (00:00:00.000)
+    const todayDate = new DateObject().set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+
+    // Create a DateObject instance for tomorrow by adding one day to todayDate
+    const tomorrowDateObject = new DateObject(todayDate).add(1, "day");
+
+    // Initialize the data used for filters, including start and end dates
     const initialData = {
         filters: [
-            {uniqId: "startDate", Property: "Date", Operator: "<=", Value: currentDate, showValue: currentDate},
-            {uniqId: "endDate", Property: "Date", Operator: ">", Value: tomorrowJsDate, showValue: tomorrowJsDate},
+            {uniqId: "startDate", Property: "Date", Operator: "<=", Value: dateObjectToIso8601(todayDate), showValue: todayDate},
+            {uniqId: "endDate", Property: "Date", Operator: ">", Value: dateObjectToIso8601(tomorrowDateObject), showValue: tomorrowDateObject.toDate()},
         ],
         tableData: [],
         requestUrl: "",
