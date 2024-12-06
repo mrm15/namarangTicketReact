0ca522c729928f0f8900585111ef789701fb7a1d
mrm15 <mrm.learn@gmail.com>
2024-12-07 02:09:58 +0330

    update

--- src/Components/TableG/FullTable/components/DateShowltr.tsx
+++ src/Components/TableG/FullTable/components/DateShowltr.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {date} from "yup";
+import {p2e} from "../../../../utils/NumericFunction.tsx";
 
-const DateShowLtr = ({info}) => {
-    const value = info.getValue()
+const DateShowLtr = ({ info }) => {
+    let value = info.getValue();
+    if (typeof value === "string" && !isNaN(Date.parse(value))) {
+        try {
+            value = new Date(value).toLocaleString("fa-IR");
+            value = p2e(value);
+        } catch (error) {
+            // مدیریت خطا در صورت بروز مشکل
+        }
+    }
     return (
         <div className={"ltr"}>
             {value}
         </div>
     );
 };
-
 export default DateShowLtr;
