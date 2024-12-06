830565c47aa33339412c84d632ff2bfd7306a656
mrm15 <mrm.learn@gmail.com>
2024-12-07 02:12:36 +0330

    update

--- src/Components/MyTicketList/MyTicketList.tsx
+++ src/Components/MyTicketList/MyTicketList.tsx
@@ -4,10 +4,17 @@ import TableG from "../TableG/TableG.tsx";
 const MyTicketList = () => {
 
     console.log("MyTicketList is Here")
+    const boldRowCondition = (row?: any) => {
+        const readStatus = row?.original?.readStatus
+        return readStatus === false;
+        //return !(row?.original?.readStatus)
+        // return false
+    }
     return (
         <div>
             <TableG
                 url={"/ticket/myTicketList"}
+                boldRowCondition={boldRowCondition}
                 />
         </div>
     );
