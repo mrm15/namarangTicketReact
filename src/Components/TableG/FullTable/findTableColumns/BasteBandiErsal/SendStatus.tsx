c8b1b4789cf9190fed4f31ad8945072e9c57270c
mohammad mirarab <mrm.learn@gmail.com>
2024-12-24 17:04:45 +0330

    update

--- src/Components/TableG/FullTable/findTableColumns/BasteBandiErsal/SendStatus.tsx
+++ src/Components/TableG/FullTable/findTableColumns/BasteBandiErsal/SendStatus.tsx
@@ -35,62 +35,15 @@ const SendStatus = ({
     const rnd = randomNumberGenerator().toString()
 
     const billNumber = info?.row?.original?.Number
-    const handleChangePack = async (e) => {
-        const orderName = info?.row?.original?.ContactTitle;
-        let statusNumber: number | string = "";
-        let date = getCurrentDate();
-
-
-        let message = "";
-        if (e.target.checked) {
-            message = `تغییر وضعیت  ${orderName} به حالت بسته بندی شده `
-            statusNumber = 8;
-
-        } else {
-            message = `تغییر وضعیت  ${orderName} به حالت بسته بندی  «نشده» `
-            statusNumber = 7;
-            date = "-"
-            // اینجا حالتیه که چک رو برداشته و اگه دسترسی به حالت قبل برگرداندن داره اجاره داره از اینجا ب بعد ادامه پیدا کنه
-            // اگه دسترسی نداره همینجا ریترن میکنم
-            const hasAccessToUnCheckedSendPackages = auth?.userInfo?.roleAccessList?.includes(ROLES.hasAccessToUnCheckedSendPackages[0])
-            if (!hasAccessToUnCheckedSendPackages) {
-                toast.dismiss()
-                toast.error("شما مجوز تغییر سفارش به حالت ارسال نشده را ندارید.")
-                return;
-            }
-
-        }
-
-
-        const isConfirm = confirm(message);
-
-        if (isConfirm) {
-            const invoice = makeInvoiceBaseOnHesabfaData(info?.row?.original)
-            const result = await myAxiosP.post("/hesabfa/updatePackStatusTo8",
-                {
-                    invoice,
-                    date,
-                    statusNumber,
-                })
-
-            if (result.status === 200) {
-                toast(result.data.message)
-                setMyData({reload: randomNumberGenerator()})
-            }
-        }
-    }
 
 
     const canSetSeeChangeBillStatusButton = auth?.userInfo?.roleAccessList?.includes("canSetSeeChangeBillStatusButton")
-    const hasAccessVerifyBill = auth?.userInfo?.roleAccessList?.includes(ROLES.saveBillAsDone[0])
-    const hasAccessDraftBill = auth?.userInfo?.roleAccessList?.includes(ROLES.saveBillAsDraft[0])
     const hasAccessToGetScreenShotBills = auth?.userInfo?.roleAccessList?.includes(ROLES.screenShotBills[0])
     const hasAccessViewBills = auth?.userInfo?.roleAccessList?.includes(ROLES.viewBills[0])
     const hasAccessToEditBillInChatList = auth?.userInfo?.roleAccessList?.includes(ROLES.editBillInChatList[0])
 
 
     const roleAccessList = auth.userInfo?.roleAccessList;
-    // const accessToEditBill = roleAccessList.includes(ROLES.editBillInChatList[0])
     const accessToDeleteBill = roleAccessList.includes(ROLES.deleteBill[0])
 
     const billObject = info?.row?.original
@@ -98,8 +51,6 @@ const SendStatus = ({
     return (
         <div className={"flex flex-wrap items-center gap-1 "}>
             {canSetSeeChangeBillStatusButton && <ChangeBillStatus setMyData={setMyData} info={info}/>}
-
-
               <div className={"flex flex-wrap gap-1"}>
                   {hasAccessViewBills && <a className={"btn-small-show"} target={"_blank"}
                                             href={PAGES.showBill + "/" + billNumber}>{"مشاهده"}</a>}
