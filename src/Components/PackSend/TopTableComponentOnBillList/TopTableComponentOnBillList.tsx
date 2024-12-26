575d1681455e6174c12b4f2c6dde034ac853eee4
mohammad mirarab <mrm.learn@gmail.com>
2024-12-26 21:40:17 +0330

    update

---
+++ src/Components/PackSend/TopTableComponentOnBillList/TopTableComponentOnBillList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DownloadTable from "./DownloadTable.tsx";
+
+const TopTableComponentOnBillList = () => {
+    return (
+        <div>
+            <DownloadTable  />
+        </div>
+    );
+};
+
+export default TopTableComponentOnBillList;
