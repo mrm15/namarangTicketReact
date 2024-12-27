20a996038272ce58cdc1a24b8ca21af91cee6f87
mohammad mirarab <mrm.learn@gmail.com>
2024-12-27 17:45:55 +0330

    update

--- src/Components/PackSend/TopTableComponentOnBillList/TopTableComponentOnBillList.tsx
+++ src/Components/PackSend/TopTableComponentOnBillList/TopTableComponentOnBillList.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import DownloadPdfErsal from "./DownloadPdfErsal.tsx";
 import DownloadExcelTable from "./DownloadExcelTable.tsx";
+import ChangeBillStatusGroup
+    from "../../TableG/FullTable/findTableColumns/BasteBandiErsal/ChangeBillStatus/ChangeBillStatusGroup.tsx";
 
 const TopTableComponentOnBillList = () => {
     return (
         <div className={"w-full flex justify-end gap-1 py-1"}>
             <DownloadPdfErsal  />
             <DownloadExcelTable  />
+            <ChangeBillStatusGroup  />
         </div>
     );
 };
