266ad2abf82883aca30bc590e8d48e4a637497df
mohammad mirarab <mrm.learn@gmail.com>
2024-12-30 22:45:48 +0330

    update

--- src/utils/playNotificationSound/playNotificationSound.tsx
+++ src/utils/playNotificationSound/playNotificationSound.tsx
@@ -5,3 +5,18 @@ export const playNotificationSound = () => {
     // const audio = new Audio('/notification.mp3'); // For public folder
     audio.play().catch((error) => console.error('Error playing sound:', error));
 };
+
+
+
+
+
+export const playNotificationSoundAsync = async () => {
+    try {
+        const audio = new Audio(notificationSound); // For imported file
+        // const audio = new Audio('/notification.mp3'); // For public folder
+        await audio.play(); // Wait for the audio to start playing
+        console.log('Notification sound is playing');
+    } catch (error) {
+        console.error('Error playing sound:', error);
+    }
+};
