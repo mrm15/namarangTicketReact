18c56cfbf51ef9805701d9b777806f17e78e55da
mohammad mirarab <mrm.learn@gmail.com>
2024-12-30 22:47:13 +0330

    update

--- src/Components/WebSocketComponent/WebSocketComponent.tsx
+++ src/Components/WebSocketComponent/WebSocketComponent.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import useWebSocket from '../../hooks/useWebSocket';
+import {playNotificationSound} from "../../utils/playNotificationSound/playNotificationSound.tsx";
 
 const WebSocketComponent = () => {
     const messages = useSelector((state: RootState) => state.websocket.messages);
     const [messageInput, setMessageInput] = useState('');
-    const { sendMessage } = useWebSocket('http://localhost:3001'); // Adjust URL to match your backend server
+    const {sendMessage} = useWebSocket('http://localhost:3001'); // Adjust URL to match your backend server
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setMessageInput(e.target.value);
@@ -14,13 +15,13 @@ const WebSocketComponent = () => {
 
     const handleSendMessage = () => {
         if (messageInput.trim()) {
-            sendMessage({ type: 'chat', message: messageInput });
+            sendMessage({type: 'chat', message: messageInput});
             setMessageInput('');
         }
     };
 
     return (
-        <div>
+        <div className={"ltr"}>
             <h1>Socket.IO Messages</h1>
             <ul>
                 {messages.map((message, index) => (
@@ -33,7 +34,8 @@ const WebSocketComponent = () => {
                 onChange={handleInputChange}
                 placeholder="Type a message"
             />
-            <button onClick={handleSendMessage}>Send Message</button>
+            <button className={"btn-submit-mir "} onClick={handleSendMessage}>Send Message</button>
+            <button className={"btn-submit-mir "} onClick={() => playNotificationSound()}>Send Message2</button>
         </div>
     );
 };
