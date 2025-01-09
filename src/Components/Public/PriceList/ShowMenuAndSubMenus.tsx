a7d52f571102dc980701ad9b4dce36b59b629a23
mohammad mirarab <mrm.learn@gmail.com>
2025-01-09 20:52:44 +0330

    update

--- src/Components/Public/PriceList/ShowMenuAndSubMenus.tsx
+++ src/Components/Public/PriceList/ShowMenuAndSubMenus.tsx
@@ -6,68 +6,82 @@ const ShowMenuAndSubMenus = ({data}) => {
 
     const totalItems = [
         {
-            title: "منوی اول", id: 1,
+            title: "ورق ها", id: 1,
             subMenu: [
-                {title: "زیر 1منوی اول", id: 1,},
-                {title: "زیر 1منوی دوم", id: 2,},
-                {title: "زیر 1منوی سوم", id: 3,},
-                {title: "زیر 1منوی چهارم", id: 4,},
-                {title: "زیر1 منوی پنجم", id: 5,},
-                {title: "زیر1 منوی ششم", id: 6,},
-                {title: "زیر1 منوی هفتم", id: 7,},
-                {title: "زیر 1منوی هشتم", id: 8,},
+                {title: "پلکسی گلاس ", id: 1,},
+                {title: "استیل", id: 2,},
+                {title: "آهن", id: 3,},
+                {title: "آلومینیوم", id: 4,},
+                {title: "پی وی سی", id: 5,},
             ],
         },
         {
-            title: "منوی دوم", id: 2,
+            title: "لبه ها", id: 2,
             subMenu: [
-                {title: "زیر 2منوی اول", id: 1,},
-                {title: "زیر 2منوی دوم", id: 2,},
-                {title: "زیر 2منوی سوم", id: 3,},
-                {title: "زیر 2منوی چهارم", id: 4,},
-                {title: "زیر2 منوی پنجم", id: 5,},
-                {title: "زیر2 منوی ششم", id: 6,},
-                {title: "زیر2 منوی هفتم", id: 7,},
-                {title: "زیر 2منوی هشتم", id: 8,},
+                {title: "چلنیوم ", id: 1,},
+                {title: "سوئدی", id: 2,},
+                {title: "پلکسی", id: 3,},
+                {title: "استیل", id: 4,},
+                {title: "آهن", id: 5,},
             ],
         },
         {
-            title: "منوی سوم", id: 3,
+            title: "چسب ها", id: 3,
             subMenu: [
-                {title: "زیر 3منوی اول", id: 1,},
-                {title: "زیر 3منوی دوم", id: 2,},
-                {title: "زیر 3منوی سوم", id: 3,},
-                {title: "زیر 3منوی چهارم", id: 4,},
-                {title: "زیر3 منوی پنجم", id: 5,},
-                {title: "زیر3 منوی ششم", id: 6,},
-                {title: "زیر3 منوی هفتم", id: 7,},
-                {title: "زیر 3منوی هشتم", id: 8,},
+                {title: "123 درجه یک ترک (متر)", id: 1,},
+                {title: "کلر فرم برای دوبل پلکسی (متر بر خط)", id: 2,},
+                {title: "چسب کلرسفت برای لبه پلاستیک (متربرخط)", id: 3,},
+                {title: "آکواریوم برای محکم کردن حروف (متربرخط)", id: 4,},
+                {title: "پولی اورتان برای محکم کردن حروف (متربرخط)", id: 5,},
+                {title: "پولی اورتان برای چسباندن استیل و فلز به پلکسی (مساحت قطعه)", id: 6,},
             ],
         },
         {
-            title: "منوی چهارم", id: 4,
+            title: "اجرت برش", id: 4,
 
             subMenu: [
-                {title: "زیر 4منوی اول", id: 1,},
-                {title: "زیر 4منوی دوم", id: 2,},
-                {title: "زیر 4منوی سوم", id: 3,},
-                {title: "زیر 4منوی چهارم", id: 4,},
-                {title: "زیر34 منوی پنجم", id: 5,},
-                {title: "زیر4 منوی ششم", id: 6,},
-                {title: "زیر4 منوی هفتم", id: 7,},
-                {title: "زیر 4منوی هشتم", id: 8,},
+                {title: "برش پلکسی ", id: 1,},
+                {title: "برش فلزات ( فایبر )", id: 2,},
+                {title: "برش سی ان سی ( مته )", id: 3,},
+                {title: "برش پی وی سی", id: 4,},
+                {title: "برش ام دی اف", id: 5,},
             ],
         },
-        {title: "منوی پنجم", id: 5,},
-        {title: "منوی ششم", id: 6,},
-        {title: "منوی هفتم", id: 7,},
-        {title: "منوی هشتم", id: 8,},
-        {title: "منوی هشتم", id: 9,},
-        {title: "منوی هشتم", id: 10,},
-        {title: "منوی هشتم", id: 11,},
-        {title: "منوی هشتم", id: 12,},
-        {title: "منوی هشتم", id: 13,},
-        {title: "منوی هشتم", id: 14,},
+        {
+            title: "اجرت ساخت", id: 5,
+            subMenu: [
+                {title: "چلنیوم آسان و سخت", id: 1,},
+                {title: "سوئدی آسان و سخت", id: 2,},
+                {title: "پلاستیک آسان و سخت و کاردستی", id: 3,},
+                {title: "+ پلاستیک مولتی بند  آسان و سخت و کاردستی", id: 4,},
+                {title: "جوش استیل آسان و سخت", id: 5,},
+                {title: "جوش آهن آسان و سخت", id: 6,},
+                {title: "جوش فایبر آسان و سخت", id: 7,},
+                {title: "المان شهری، آسان و سخت و کاردستی", id: 8,},
+            ],
+
+        },
+        {
+            title: "دوبل و خدمات  متفرقه", id: 6,
+            subMenu: [
+                {title: "دوبل پلکسی", id: 1,},
+                {title: "پوست کن کردن لبه و پلکسی و سلفون کردن روی کار", id: 2,},
+                {title: "پلات نصب ( متر طول کاغذ )", id: 3,},
+                {title: "پیچ نصب حروف به تعداد لازم", id: 4,},
+                {title: "تغییر سیم سیم کشی به 50 سانتی متری", id: 5,},
+                {title: "تغییر سیم سیم کشی به 75 سانتی متری", id: 6,},
+                {title: "تغییر سیم سیم کشی به 100 سانتی متری", id: 7,},
+            ],
+        },
+        {
+            title: "اس ام دی ( نور)", id: 7,
+            subMenu: [
+                {title: "اس ام دی رزین چهار مقاومت ", id: 1,},
+                {title: "اس ام دی اینجکشن اکونومی", id: 2,},
+                {title: "اس ام دی اینجشمن سامسونگ", id: 3,},
+                {title: "اس ام دی اینجشن 220 ولت", id: 4,},
+            ],
+        },
     ]
     const [selectedMenuId, setSelectedMenuId] = useObjectDataHolder({
         menuId: 1,
@@ -83,11 +97,11 @@ const ShowMenuAndSubMenus = ({data}) => {
             currentSubmenu.scrollIntoView({behavior: "smooth", block: "nearest"});
         }
 
-        const rnd = Math.floor(Math.random()*1000)
+        const rnd = Math.floor(Math.random() * 1000)
         const currentItemsRef = itemsRef.current[rnd];
-        if(currentItemsRef){
+        if (currentItemsRef) {
             console.log(rnd)
-            currentItemsRef.scrollIntoView({behavior: "smooth" , block:"start"});
+            currentItemsRef.scrollIntoView({behavior: "smooth", block: "start"});
         }
     }, [selectedMenuId.subMenuId, selectedMenuId.menuId]);
 
@@ -95,10 +109,12 @@ const ShowMenuAndSubMenus = ({data}) => {
     try {
         return (
             <div>
-                <div className="w-full lg:w-1024 mx-auto h-screen p-2">
-                    <div style={{
-                        height: "10vh",
-                    }}>
+                <div className="w-full lg:w-1024 mx-auto  ">
+                    <div
+                        className={"bg-blue-500"}
+                        style={{
+                            height: "10vh",
+                        }}>
                         <div
                             className="flex gap-1 overflow-x-scroll flex-nowrap w-full"
                             style={{overflowY: "hidden"}}
@@ -148,15 +164,28 @@ const ShowMenuAndSubMenus = ({data}) => {
                     <div
                         className={"overflow-scroll"}
                         style={{
-                            height: "90vh",
+                            height: "70vh",
 
                         }}
                     >
                         <div>
                             <div className="">
-                                {data?.data?.List?.map((product) => (
-                                    <div
-                                        ref={(el) => (itemsRef.current[product.Id] = el) }
+                                {data?.map((product, index) => {
+
+
+                                    if (product?.type === "title") {
+
+                                        return <div key={index + 8520000}
+                                                    ref={(el) => (itemsRef.current[product.Id] = el)}
+                                        >
+                                            <div className={"bg-green-400 px-2"}>
+                                                {product?.title}
+                                            </div>
+
+                                        </div>
+                                    }
+                                    return <div
+
                                         key={product.Id}
                                         className="bg-white shadow rounded  p-4 border border-gray-200 my-1"
                                     >
@@ -184,7 +213,7 @@ const ShowMenuAndSubMenus = ({data}) => {
                                             </p>
                                         </div>
                                     </div>
-                                ))}
+                                })}
                             </div>
                         </div>
                     </div>
@@ -199,5 +228,4 @@ const ShowMenuAndSubMenus = ({data}) => {
         return <>{error.toString()}</>
     }
 };
-
 export default ShowMenuAndSubMenus;
