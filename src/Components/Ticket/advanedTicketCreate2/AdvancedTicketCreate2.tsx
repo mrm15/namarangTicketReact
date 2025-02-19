80d0f2293f88a06e2c56aafee5864726fcc93956
mohammad mirarab <mrm.learn@gmail.com>
2025-02-19 14:51:55 +0330

    update

--- src/Components/Ticket/advanedTicketCreate2/AdvancedTicketCreate2.tsx
+++ src/Components/Ticket/advanedTicketCreate2/AdvancedTicketCreate2.tsx
@@ -1,33 +1,9 @@
 import React from 'react';
 import {AdvancedTicketContextProvider} from "./AdvancedTicketContext";
 import AdvancedTicketCreateComponent from "./AdvancedTicketCreateComponent/AdvancedTicketCreateComponent";
+import {initialDataAdvancedTicketCreate2} from "./AdvancedTicketTypes.tsx";
 
 const AdvancedTicketCreate2 = () => {
-
-
-    const initialDataAdvancedTicketCreate2 = {
-        // عنوان سفارش
-        title: '',
-        // توضیحات سفارش
-        description: '',
-        // فایل نهایی یا فایل های نهایی
-        files: [],
-        filesUploadId: [],
-        // فایل رو کی فرستاده و قراره توی پنل کی نشون بدیم که این فایل مال تو بوده ینی فرستنده فایل
-        senderUserId: '',
-        senderUserData: "",
-        // لیست کاربران
-        userList: [],
-        // ماکزیمم فایل سایزی که میشه آپلود کرد.
-        maxFileSize: 0,
-        // فایل های شات
-        screenShot: [],
-        screenShotUploadId: [],
-        // آیا پروسه داره آپلود میشه؟
-        isSendingRequest: false,
-    }
-
-
     return (
         <AdvancedTicketContextProvider initialData={initialDataAdvancedTicketCreate2}>
             <AdvancedTicketCreateComponent/>
